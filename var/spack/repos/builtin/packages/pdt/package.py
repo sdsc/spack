sdsc/spack

commit 53621f8a2f8dfcf0e5ea3581bdd1f82303f8e78b

Massive conversion from Package to AutotoolsPackage (#2845)
* Massive conversion from Package to AutotoolsPackage

* Forgot to convert p4est to AutotoolsPackage

* Fix typo

* Fix broken link in docs

var/spack/repos/builtin/packages/pdt/package.py
@@ -25,7 +25,7 @@
 from spack import *
 
 
-class Pdt(Package):
+class Pdt(AutotoolsPackage):
     """Program Database Toolkit (PDT) is a framework for analyzing source
        code written in several programming languages and for making rich
        program knowledge accessible to developers of static and dynamic
@@ -43,8 +43,3 @@ class Pdt(Package):
     version('3.20',   'c3edabe202926abe04552e33cd39672d')
     version('3.19',   '5c5e1e6607086aa13bf4b1b9befc5864')
     version('3.18.1', 'e401534f5c476c3e77f05b7f73b6c4f2')
-
-    def install(self, spec, prefix):
-        configure('-prefix=%s' % prefix)
-        make()
-        make('install')

var/spack/repos/builtin/packages/applewmproto/package.py
@@ -25,7 +25,7 @@
 from spack import *
 
 
-class Applewmproto(Package):
+class Applewmproto(AutotoolsPackage):
     """Apple Rootless Window Management Extension.
 
     This extension defines a protcol that allows X window managers
@@ -39,8 +39,3 @@ class Applewmproto(Package):
 
     depends_on('pkg-config@0.9.0:', type='build')
     depends_on('util-macros', type='build')
-
-    def install(self, spec, prefix):
-        configure('--prefix={0}'.format(prefix))
-
-        make('install')

var/spack/repos/builtin/packages/appres/package.py
@@ -25,7 +25,7 @@
 from spack import *
 
 
-class Appres(Package):
+class Appres(AutotoolsPackage):
     """The appres program prints the resources seen by an application (or
     subhierarchy of an application) with the specified class and instance
     names.  It can be used to determine which resources a particular
@@ -42,9 +42,3 @@ class Appres(Package):
     depends_on('xproto@7.0.17:', type='build')
     depends_on('pkg-config@0.9.0:', type='build')
     depends_on('util-macros', type='build')
-
-    def install(self, spec, prefix):
-        configure('--prefix={0}'.format(prefix))
-
-        make()
-        make('install')

var/spack/repos/builtin/packages/asciidoc/package.py
@@ -25,7 +25,7 @@
 from spack import *
 
 
-class Asciidoc(Package):
+class Asciidoc(AutotoolsPackage):
     """A presentable text document format for writing articles, UNIX man
     pages and other small to medium sized documents."""
 
@@ -38,9 +38,3 @@ class Asciidoc(Package):
     depends_on('libxslt')
     depends_on('docbook-xml')
     depends_on('docbook-xsl')
-
-    def install(self, spec, prefix):
-        configure('--prefix=%s' % prefix)
-
-        make()
-        make("install")

var/spack/repos/builtin/packages/atk/package.py
@@ -25,7 +25,7 @@
 from spack import *
 
 
-class Atk(Package):
+class Atk(AutotoolsPackage):
     """ATK provides the set of accessibility interfaces that are
        implemented by other toolkits and applications. Using the ATK
        interfaces, accessibility tools have full access to view and
@@ -43,8 +43,3 @@ class Atk(Package):
         """Handle atk's version-based custom URLs."""
         url = 'http://ftp.gnome.org/pub/gnome/sources/atk'
         return url + '/%s/atk-%s.tar.xz' % (version.up_to(2), version)
-
-    def install(self, spec, prefix):
-        configure("--prefix=%s" % prefix)
-        make()
-        make("install")

var/spack/repos/builtin/packages/bdftopcf/package.py
@@ -25,7 +25,7 @@
 from spack import *
 
 
-class Bdftopcf(Package):
+class Bdftopcf(AutotoolsPackage):
     """bdftopcf is a font compiler for the X server and font server.  Fonts
     in Portable Compiled Format can be read by any architecture, although
     the file is structured to allow one particular architecture to read
@@ -42,9 +42,3 @@ class Bdftopcf(Package):
 
     depends_on('pkg-config@0.9.0:', type='build')
     depends_on('util-macros', type='build')
-
-    def install(self, spec, prefix):
-        configure('--prefix={0}'.format(prefix))
-
-        make()
-        make('install')

var/spack/repos/builtin/packages/beforelight/package.py
@@ -25,7 +25,7 @@
 from spack import *
 
 
-class Beforelight(Package):
+class Beforelight(AutotoolsPackage):
     """The beforelight program is a sample implementation of a screen saver
     for X servers supporting the MIT-SCREEN-SAVER extension.   It is only
     recommended for use as a code sample, as it does not include features
@@ -42,9 +42,3 @@ class Beforelight(Package):
 
     depends_on('pkg-config@0.9.0:', type='build')
     depends_on('util-macros', type='build')
-
-    def install(self, spec, prefix):
-        configure('--prefix={0}'.format(prefix))
-
-        make()
-        make('install')

var/spack/repos/builtin/packages/bertini/package.py
@@ -25,7 +25,7 @@
 from spack import *
 
 
-class Bertini(Package):
+class Bertini(AutotoolsPackage):
     """Bertini is a general-purpose solver, written in C, that was created
     for research about polynomial continuation. It solves for the numerical
     solution of systems of polynomial equations using homotopy continuation."""
@@ -42,9 +42,3 @@ class Bertini(Package):
     depends_on('gmp')
     depends_on('mpfr')
     depends_on('mpi', when='+mpi')
-
-    def install(self, spec, prefix):
-        configure('--prefix=%s' % prefix)
-
-        make()
-        make("install")

commit 0653074e577f5d3d1accc1ac7af452c2005d0127

Bug fix: PDT configure uses -prefix, not --prefix (#2976)
* This bug was introduced in #2845.

var/spack/repos/builtin/packages/pdt/package.py
@@ -43,3 +43,6 @@ class Pdt(AutotoolsPackage):
     version('3.20',   'c3edabe202926abe04552e33cd39672d')
     version('3.19',   '5c5e1e6607086aa13bf4b1b9befc5864')
     version('3.18.1', 'e401534f5c476c3e77f05b7f73b6c4f2')
+
+    def configure(self, spec, prefix):
+        configure('-prefix=%s' % prefix)