sdsc/spack

commit 11219bc0d2a19e387b91142f966385978e108a9f

install_tree, copy_tree can install into existing directory structures (#8289)
Replace use of `shutil.copytree` with `copy_tree` and `install_tree` functions in `llnl.util.filesystem`.

- `copy_tree` copies without setting permissions.  It should be used to copy files around in the build directory.
- `install_tree` copies files and sets permissions.  It should be used to copy files into the installation directory.
- `install` and `copy` are analogous single-file functions.
- add more extensive tests for these functions
- update packages to use these functions.

var/spack/repos/builtin/packages/leveldb/package.py
@@ -26,7 +26,7 @@ import glob
 from spack import *
 
 
-class Leveldb(Package):
+class Leveldb(MakefilePackage):
     """LevelDB is a fast key-value storage library written at Google
     that provides an ordered mapping from string keys to string values."""
 
@@ -39,21 +39,14 @@ class Leveldb(Package):
     depends_on("snappy")
 
     def install(self, spec, prefix):
-        make()
+        mkdirp(prefix.lib.pkgconfig)
 
-        mkdirp(prefix.include)
-        mkdirp(prefix.lib)
-        mkdirp(join_path(prefix.lib, 'pkgconfig'))
+        libraries  = glob.glob('out-shared/libleveldb.*')
+        libraries += glob.glob('out-static/libleveldb.*')
+        for library in libraries:
+            install(library, prefix.lib)
 
-        cp = which('cp')
-
-        # cp --preserve=links libleveldb.* prefix/lib
-        args = glob.glob('out-shared/libleveldb.*') \
-            + glob.glob('out-static/libleveldb.*')
-        args.append(prefix.lib)
-        cp('--preserve=links', *args)
-
-        cp('-r', 'include/leveldb', prefix.include)
+        install_tree('include/leveldb', prefix.include)
 
         with open(join_path(prefix.lib, 'pkgconfig', 'leveldb.pc'), 'w') as f:
             f.write('prefix={0}\n'.format(prefix))

var/spack/repos/builtin/packages/alglib/package.py
@@ -26,7 +26,6 @@ from spack import *
 import glob
 import os
 import sys
-import shutil
 
 
 class Alglib(MakefilePackage):
@@ -48,7 +47,7 @@ class Alglib(MakefilePackage):
         make_file_src = join_path(os.path.dirname(self.module.__file__),
                                   'Makefile')
         make_file = join_path(self.stage.source_path, 'src', 'Makefile')
-        shutil.copy(make_file_src, make_file)
+        copy(make_file_src, make_file)
         filter_file(r'so', dso_suffix, make_file)
 
     def install(self, spec, prefix):

var/spack/repos/builtin/packages/arlecore/package.py
@@ -23,7 +23,6 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
 from spack import *
-import distutils.dir_util
 
 
 class Arlecore(Package):
@@ -37,4 +36,4 @@ class Arlecore(Package):
     depends_on('r', type=('build', 'run'))
 
     def install(self, spec, prefix):
-        distutils.dir_util.copy_tree(".", prefix)
+        install_tree('.', prefix)

var/spack/repos/builtin/packages/atom-dft/package.py
@@ -22,9 +22,7 @@
 # License along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
-
 from spack import *
-import shutil
 
 
 class AtomDft(MakefilePackage):
@@ -40,7 +38,7 @@ class AtomDft(MakefilePackage):
     depends_on('xmlf90')
 
     def edit(self, spec, prefix):
-        shutil.copyfile('arch.make.sample', 'arch.make')
+        copy('arch.make.sample', 'arch.make')
 
     @property
     def build_targets(self):

var/spack/repos/builtin/packages/autodock-vina/package.py
@@ -23,8 +23,6 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
 from spack import *
-from shutil import copyfile
-from shutil import copymode
 import sys
 
 
@@ -66,7 +64,5 @@ class AutodockVina(MakefilePackage):
     def install(self, spec, prefix):
         with working_dir(self.build_directory):
             mkdirp(prefix.bin)
-            copyfile("vina", join_path(prefix.bin, "vina"))
-            copymode("vina", join_path(prefix.bin, "vina"))
-            copyfile("vina_split", join_path(prefix.bin, "vina_split"))
-            copymode("vina_split", join_path(prefix.bin, "vina_split"))
+            install('vina', prefix.bin)
+            install('vina_split', prefix.bin)

var/spack/repos/builtin/packages/bcl2fastq2/package.py
@@ -24,7 +24,6 @@
 ##############################################################################
 from spack import *
 import os
-import shutil
 import glob
 import llnl.util.tty as tty
 
@@ -94,7 +93,7 @@ class Bcl2fastq2(Package):
                     tty.msg("cwd sez: {0}".format(os.getcwd()))
                     tarball = glob.glob(join_path('spack-expanded-archive',
                                         'bcl2fastq2*.tar.gz'))[0]
-                    shutil.move(tarball, '.')
+                    copy(tarball, '.')
                     os.rmdir('spack-expanded-archive')
                     tar = which('tar')
                     tarball = os.path.basename(tarball)

var/spack/repos/builtin/packages/bioawk/package.py
@@ -23,8 +23,6 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
 from spack import *
-from shutil import copyfile
-from shutil import copymode
 
 
 class Bioawk(MakefilePackage):
@@ -45,7 +43,5 @@ class Bioawk(MakefilePackage):
 
     def install(self, spec, prefix):
         mkdirp(prefix.bin)
-        copyfile("bioawk", join_path(prefix.bin, "bioawk"))
-        copymode("bioawk", join_path(prefix.bin, "bioawk"))
-        copyfile("maketab", join_path(prefix.bin, "maketab"))
-        copymode("maketab", join_path(prefix.bin, "maketab"))
+        install('bioawk',  prefix.bin)
+        install('maketab', prefix.bin)

var/spack/repos/builtin/packages/biopieces/package.py
@@ -23,7 +23,6 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
 from spack import *
-import distutils.dir_util
 
 
 class Biopieces(Package):
@@ -79,7 +78,7 @@ class Biopieces(Package):
     depends_on('scan-for-matches')
 
     def install(self, spec, prefix):
-        distutils.dir_util.copy_tree(".", prefix)
+        install_tree('.', prefix)
 
     def setup_environment(self, spack_env, run_env):
         # Note: user will need to set environment variables on their own,

commit 00b489106598b1c75f585bf1741ea57e7dfbe5dd

leveldb: Fix header installation (#9069)
leveldb headers are typically included via <leveldb/c.h>. The migration
to install_tree in 11219bc0d2a19e387b91142f966385978e108a9f caused the
headers to end up in prefix.include, which breaks existing applications.

var/spack/repos/builtin/packages/leveldb/package.py
@@ -46,7 +46,7 @@ class Leveldb(MakefilePackage):
         for library in libraries:
             install(library, prefix.lib)
 
-        install_tree('include/leveldb', prefix.include)
+        install_tree('include', prefix.include)
 
         with open(join_path(prefix.lib, 'pkgconfig', 'leveldb.pc'), 'w') as f:
             f.write('prefix={0}\n'.format(prefix))