sdsc/spack

commit 72b89216da3391a1b1eb5d592a0576637d76e7aa

macos: update build process to use spawn instead of fork (#18205)
Spack creates a separate process to do package installation. Different
operating systems and Python versions use different methods to create
it but up until Python 3.8 both Linux and Mac OS used "fork" (which
duplicates process memory, file descriptor table, etc.).

Python >= 3.8 on Mac OS prefers creating an entirely new process
(referred to as the "spawn" start method) because "fork" was found to
cause issues (in other words "spawn" is the default start method used
by multiprocessing.Process). Spack was dependent on the particular
behavior of fork to replicate process memory and transmit file
descriptors.

This PR refactors the Spack internals to support starting a child
process with the "spawn" method. To achieve this, it makes the
following changes:

- ensure that the package repository and other global state are
  transmitted to the child process
- ensure that file descriptors are transmitted to the child process in
  a way that works with multiprocessing and spawn
- make all the state needed for the build process and tests picklable
  (package, stage, etc.)
- move a number of locally-defined functions into global scope so that
  they can be pickled
- rework tests where needed to avoid using local functions

This PR also reworks sbang tests to work on macOS, where temporary
directories are deeper than the Linux sbang limit. We make the limit
platform-dependent (macOS supports 512-character shebangs)

See: #14102

lib/spack/spack/subprocess_context.py
@@ -1,0 +1,149 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+"""
+This module handles transmission of Spack state to child processes started
+using the 'spawn' start method. Notably, installations are performed in a
+subprocess and require transmitting the Package object (in such a way
+that the repository is available for importing when it is deserialized);
+installations performed in Spack unit tests may include additional
+modifications to global state in memory that must be replicated in the
+child process.
+"""
+
+from types import ModuleType
+
+import pickle
+import pydoc
+import io
+import sys
+import multiprocessing
+
+import spack.architecture
+import spack.config
+
+
+_serialize = sys.version_info >= (3, 8) and sys.platform == 'darwin'
+
+
+patches = None
+
+
+def append_patch(patch):
+    global patches
+    if not patches:
+        patches = list()
+    patches.append(patch)
+
+
+def serialize(obj):
+    serialized_obj = io.BytesIO()
+    pickle.dump(obj, serialized_obj)
+    serialized_obj.seek(0)
+    return serialized_obj
+
+
+class SpackTestProcess(object):
+    def __init__(self, fn):
+        self.fn = fn
+
+    def _restore_and_run(self, fn, test_state):
+        test_state.restore()
+        fn()
+
+    def create(self):
+        test_state = TestState()
+        return multiprocessing.Process(
+            target=self._restore_and_run,
+            args=(self.fn, test_state))
+
+
+class PackageInstallContext(object):
+    """Captures the in-memory process state of a package installation that
+    needs to be transmitted to a child process.
+    """
+    def __init__(self, pkg):
+        if _serialize:
+            self.serialized_pkg = serialize(pkg)
+        else:
+            self.pkg = pkg
+        self.test_state = TestState()
+
+    def restore(self):
+        self.test_state.restore()
+        if _serialize:
+            return pickle.load(self.serialized_pkg)
+        else:
+            return self.pkg
+
+
+class TestState(object):
+    """Spack tests may modify state that is normally read from disk in memory;
+    this object is responsible for properly serializing that state to be
+    applied to a subprocess. This isn't needed outside of a testing environment
+    but this logic is designed to behave the same inside or outside of tests.
+    """
+    def __init__(self):
+        if _serialize:
+            self.repo_dirs = list(r.root for r in spack.repo.path.repos)
+            self.config = spack.config.config
+            self.platform = spack.architecture.platform
+            self.test_patches = store_patches()
+
+            # TODO: transfer spack.store.store? note that you should not
+            # transfer spack.store.store and spack.store.db: 'db' is a
+            # shortcut that accesses the store (so transferring both can
+            # create an inconsistency). Some tests set 'db' directly, and
+            # others set 'store'
+
+    def restore(self):
+        if _serialize:
+            spack.repo.path = spack.repo._path(self.repo_dirs)
+            spack.config.config = self.config
+            spack.architecture.platform = self.platform
+            self.test_patches.restore()
+
+
+class TestPatches(object):
+    def __init__(self, module_patches, class_patches):
+        self.module_patches = list(
+            (x, y, serialize(z)) for (x, y, z) in module_patches)
+        self.class_patches = list(
+            (x, y, serialize(z)) for (x, y, z) in class_patches)
+
+    def restore(self):
+        for module_name, attr_name, value in self.module_patches:
+            value = pickle.load(value)
+            module = __import__(module_name)
+            setattr(module, attr_name, value)
+        for class_fqn, attr_name, value in self.class_patches:
+            value = pickle.load(value)
+            cls = pydoc.locate(class_fqn)
+            setattr(cls, attr_name, value)
+
+
+def store_patches():
+    global patches
+    module_patches = list()
+    class_patches = list()
+    if not patches:
+        return TestPatches(list(), list())
+    for patch in patches:
+        for target, name, _ in patches:
+            if isinstance(target, ModuleType):
+                new_val = getattr(target, name)
+                module_name = target.__name__
+                module_patches.append((module_name, name, new_val))
+            elif isinstance(target, type):
+                new_val = getattr(target, name)
+                class_fqn = '.'.join([target.__module__, target.__name__])
+                class_patches.append((class_fqn, name, new_val))
+
+    return TestPatches(module_patches, class_patches)
+
+
+def clear_patches():
+    global patches
+    patches = None

.github/workflows/macos_unit_tests.yaml
@@ -12,13 +12,16 @@ on:
 jobs:
   build:
     runs-on: macos-latest
+    strategy:
+      matrix:
+        python-version: [3.8, 3.9]
     steps:
     - uses: actions/checkout@v2
       with:
         fetch-depth: 0
     - uses: actions/setup-python@v2
       with:
-        python-version: 3.9
+        python-version: ${{ matrix.python-version }}
     - name: Install Python packages
       run: |
           pip install --upgrade pip six setuptools

bin/spack
@@ -59,6 +59,8 @@ if 'ruamel.yaml' in sys.modules:
 if 'ruamel' in sys.modules:
     del sys.modules['ruamel']
 
-# Once we've set up the system path, run the spack main method
 import spack.main  # noqa
-sys.exit(spack.main.main())
+
+# Once we've set up the system path, run the spack main method
+if __name__ == "__main__":
+    sys.exit(spack.main.main())

lib/spack/llnl/util/lang.py
@@ -568,6 +568,12 @@ class Singleton(object):
         return self._instance
 
     def __getattr__(self, name):
+        # When unpickling Singleton objects, the 'instance' attribute may be
+        # requested but not yet set. The final 'getattr' line here requires
+        # 'instance'/'_instance' to be defined or it will enter an infinite
+        # loop, so protect against that here.
+        if name in ['_instance', 'instance']:
+            raise AttributeError()
         return getattr(self.instance, name)
 
     def __getitem__(self, name):
@@ -596,6 +602,8 @@ class LazyReference(object):
         self.ref_function = ref_function
 
     def __getattr__(self, name):
+        if name == 'ref_function':
+            raise AttributeError()
         return getattr(self.ref_function(), name)
 
     def __getitem__(self, name):

lib/spack/llnl/util/tty/log.py
@@ -21,7 +21,6 @@ from six import string_types
 from six import StringIO
 
 import llnl.util.tty as tty
-from llnl.util.lang import fork_context
 
 try:
     import termios
@@ -237,6 +236,8 @@ class keyboard_input(object):
         """If termios was available, restore old settings."""
         if self.old_cfg:
             self._restore_default_terminal_settings()
+            if sys.version_info >= (3,):
+                atexit.unregister(self._restore_default_terminal_settings)
 
         # restore SIGSTP and SIGCONT handlers
         if self.old_handlers:
@@ -288,6 +289,109 @@ def _file_descriptors_work(*streams):
         return False
 
 
+class FileWrapper(object):
+    """Represents a file. Can be an open stream, a path to a file (not opened
+    yet), or neither. When unwrapped, it returns an open file (or file-like)
+    object.
+    """
+    def __init__(self, file_like):
+        # This records whether the file-like object returned by "unwrap" is
+        # purely in-memory. In that case a subprocess will need to explicitly
+        # transmit the contents to the parent.
+        self.write_in_parent = False
+
+        self.file_like = file_like
+
+        if isinstance(file_like, string_types):
+            self.open = True
+        elif _file_descriptors_work(file_like):
+            self.open = False
+        else:
+            self.file_like = None
+            self.open = True
+            self.write_in_parent = True
+
+        self.file = None
+
+    def unwrap(self):
+        if self.open:
+            if self.file_like:
+                self.file = open(self.file_like, 'w')
+            else:
+                self.file = StringIO()
+            return self.file
+        else:
+            # We were handed an already-open file object. In this case we also
+            # will not actually close the object when requested to.
+            return self.file_like
+
+    def close(self):
+        if self.file:
+            self.file.close()
+
+
+class MultiProcessFd(object):
+    """Return an object which stores a file descriptor and can be passed as an
+       argument to a function run with ``multiprocessing.Process``, such that
+       the file descriptor is available in the subprocess."""
+    def __init__(self, fd):
+        self._connection = None
+        self._fd = None
+        if sys.version_info >= (3, 8):
+            self._connection = multiprocessing.connection.Connection(fd)
+        else:
+            self._fd = fd
+
+    @property
+    def fd(self):
+        if self._connection:
+            return self._connection._handle
+        else:
+            return self._fd
+
+    def close(self):
+        if self._connection:
+            self._connection.close()
+        else:
+            os.close(self._fd)
+
+
+def close_connection_and_file(multiprocess_fd, file):
+    # MultiprocessFd is intended to transmit a FD
+    # to a child process, this FD is then opened to a Python File object
+    # (using fdopen). In >= 3.8, MultiprocessFd encapsulates a
+    # multiprocessing.connection.Connection; Connection closes the FD
+    # when it is deleted, and prints a warning about duplicate closure if
+    # it is not explicitly closed. In < 3.8, MultiprocessFd encapsulates a
+    # simple FD; closing the FD here appears to conflict with
+    # closure of the File object (in < 3.8 that is). Therefore this needs
+    # to choose whether to close the File or the Connection.
+    if sys.version_info >= (3, 8):
+        multiprocess_fd.close()
+    else:
+        file.close()
+
+
+@contextmanager
+def replace_environment(env):
+    """Replace the current environment (`os.environ`) with `env`.
+
+    If `env` is empty (or None), this unsets all current environment
+    variables.
+    """
+    env = env or {}
+    old_env = os.environ.copy()
+    try:
+        os.environ.clear()
+        for name, val in env.items():
+            os.environ[name] = val
+        yield
+    finally:
+        os.environ.clear()
+        for name, val in old_env.items():
+            os.environ[name] = val
+
+
 class log_output(object):
     """Context manager that logs its output to a file.
 
@@ -324,7 +428,8 @@ class log_output(object):
     work within test frameworks like nose and pytest.
     """
 
-    def __init__(self, file_like=None, echo=False, debug=0, buffer=False):
+    def __init__(self, file_like=None, echo=False, debug=0, buffer=False,
+                 env=None):
         """Create a new output log context manager.
 
         Args:
@@ -352,6 +457,7 @@ class log_output(object):
         self.echo = echo
         self.debug = debug
         self.buffer = buffer
+        self.env = env  # the environment to use for _writer_daemon
 
         self._active = False  # used to prevent re-entry
 
@@ -393,18 +499,7 @@ class log_output(object):
                 "file argument must be set by either __init__ or __call__")
 
         # set up a stream for the daemon to write to
-        self.close_log_in_parent = True
-        self.write_log_in_parent = False
-        if isinstance(self.file_like, string_types):
-            self.log_file = open(self.file_like, 'w')
-
-        elif _file_descriptors_work(self.file_like):
-            self.log_file = self.file_like
-            self.close_log_in_parent = False
-
-        else:
-            self.log_file = StringIO()
-            self.write_log_in_parent = True
+        self.log_file = FileWrapper(self.file_like)
 
         # record parent color settings before redirecting.  We do this
         # because color output depends on whether the *original* stdout
@@ -419,6 +514,8 @@ class log_output(object):
         # OS-level pipe for redirecting output to logger
         read_fd, write_fd = os.pipe()
 
+        read_multiprocess_fd = MultiProcessFd(read_fd)
+
         # Multiprocessing pipe for communication back from the daemon
         # Currently only used to save echo value between uses
         self.parent_pipe, child_pipe = multiprocessing.Pipe()
@@ -427,24 +524,28 @@ class log_output(object):
         try:
             # need to pass this b/c multiprocessing closes stdin in child.
             try:
-                input_stream = os.fdopen(os.dup(sys.stdin.fileno()))
-            except BaseException:
-                input_stream = None  # just don't forward input if this fails
-
-            self.process = fork_context.Process(
-                target=_writer_daemon,
-                args=(
-                    input_stream, read_fd, write_fd, self.echo, self.log_file,
-                    child_pipe
+                input_multiprocess_fd = MultiProcessFd(
+                    os.dup(sys.stdin.fileno())
                 )
-            )
-            self.process.daemon = True  # must set before start()
-            self.process.start()
-            os.close(read_fd)  # close in the parent process
+            except BaseException:
+                # just don't forward input if this fails
+                input_multiprocess_fd = None
+
+            with replace_environment(self.env):
+                self.process = multiprocessing.Process(
+                    target=_writer_daemon,
+                    args=(
+                        input_multiprocess_fd, read_multiprocess_fd, write_fd,
+                        self.echo, self.log_file, child_pipe
+                    )
+                )
+                self.process.daemon = True  # must set before start()
+                self.process.start()
 
         finally:
-            if input_stream:
-                input_stream.close()
+            if input_multiprocess_fd:
+                input_multiprocess_fd.close()
+            read_multiprocess_fd.close()
 
         # Flush immediately before redirecting so that anything buffered
         # goes to the original stream
@@ -515,18 +616,21 @@ class log_output(object):
             sys.stderr = self._saved_stderr
 
         # print log contents in parent if needed.
-        if self.write_log_in_parent:
+        if self.log_file.write_in_parent:
             string = self.parent_pipe.recv()
             self.file_like.write(string)
 
-        if self.close_log_in_parent:
-            self.log_file.close()
-
         # recover and store echo settings from the child before it dies
-        self.echo = self.parent_pipe.recv()
+        try:
+            self.echo = self.parent_pipe.recv()
+        except EOFError:
+            # This may occur if some exception prematurely terminates the
+            # _writer_daemon. An exception will have already been generated.
+            pass
 
-        # join the daemon process. The daemon will quit automatically
-        # when the write pipe is closed; we just wait for it here.
+        # now that the write pipe is closed (in this __exit__, when we restore
+        # stdout with dup2), the logger daemon process loop will terminate. We
+        # wait for that here.
         self.process.join()
 
         # restore old color and debug settings
@@ -555,7 +659,8 @@ class log_output(object):
             sys.stdout.flush()
 
 
-def _writer_daemon(stdin, read_fd, write_fd, echo, log_file, control_pipe):
+def _writer_daemon(stdin_multiprocess_fd, read_multiprocess_fd, write_fd, echo,
+                   log_file_wrapper, control_pipe):
     """Daemon used by ``log_output`` to write to a log file and to ``stdout``.
 
     The daemon receives output from the parent process and writes it both
@@ -592,25 +697,38 @@ def _writer_daemon(stdin, read_fd, write_fd, echo, log_file, control_pipe):
     ``StringIO`` in the parent. This is mainly for testing.
 
     Arguments:
-        stdin (stream): input from the terminal
-        read_fd (int): pipe for reading from parent's redirected stdout
-        write_fd (int): parent's end of the pipe will write to (will be
-            immediately closed by the writer daemon)
+        stdin_multiprocess_fd (int): input from the terminal
+        read_multiprocess_fd (int): pipe for reading from parent's redirected
+            stdout
         echo (bool): initial echo setting -- controlled by user and
             preserved across multiple writer daemons
-        log_file (file-like): file to log all output
+        log_file_wrapper (FileWrapper): file to log all output
         control_pipe (Pipe): multiprocessing pipe on which to send control
             information to the parent
 
     """
+    # If this process was forked, then it will inherit file descriptors from
+    # the parent process. This process depends on closing all instances of
+    # write_fd to terminate the reading loop, so we close the file descriptor
+    # here. Forking is the process spawning method everywhere except Mac OS
+    # for Python >= 3.8 and on Windows
+    if sys.version_info < (3, 8) or sys.platform != 'darwin':
+        os.close(write_fd)
+
     # Use line buffering (3rd param = 1) since Python 3 has a bug
     # that prevents unbuffered text I/O.
-    in_pipe = os.fdopen(read_fd, 'r', 1)
-    os.close(write_fd)
+    in_pipe = os.fdopen(read_multiprocess_fd.fd, 'r', 1)
+
+    if stdin_multiprocess_fd:
+        stdin = os.fdopen(stdin_multiprocess_fd.fd)
+    else:
+        stdin = None
 
     # list of streams to select from
     istreams = [in_pipe, stdin] if stdin else [in_pipe]
     force_echo = False      # parent can force echo for certain output
+
+    log_file = log_file_wrapper.unwrap()
 
     try:
         with keyboard_input(stdin) as kb:
@@ -672,10 +790,13 @@ def _writer_daemon(stdin, read_fd, write_fd, echo, log_file, control_pipe):
         # send written data back to parent if we used a StringIO
         if isinstance(log_file, StringIO):
             control_pipe.send(log_file.getvalue())
-        log_file.close()
+        log_file_wrapper.close()
+        close_connection_and_file(read_multiprocess_fd, in_pipe)
+        if stdin_multiprocess_fd:
+            close_connection_and_file(stdin_multiprocess_fd, stdin)
 
-    # send echo value back to the parent so it can be preserved.
-    control_pipe.send(echo)
+        # send echo value back to the parent so it can be preserved.
+        control_pipe.send(echo)
 
 
 def _retry(function):

lib/spack/llnl/util/tty/pty.py
@@ -24,7 +24,6 @@ import time
 import traceback
 
 import llnl.util.tty.log as log
-from llnl.util.lang import fork_context
 
 from spack.util.executable import which
 
@@ -234,7 +233,7 @@ class PseudoShell(object):
         ``minion_function``.
 
         """
-        self.proc = fork_context.Process(
+        self.proc = multiprocessing.Process(
             target=PseudoShell._set_up_and_run_controller_function,
             args=(self.controller_function, self.minion_function,
                   self.controller_timeout, self.sleep_time),

commit 54718038c66356cc0ee5907f5fbccfeadd822c9f

Debugging support: fix compiler wrapper log on Mac OS (#20333)
This fixes a logging error observed on macOS 11.0.1 (Big Sur).
When performing a Spack install in debugging mode (e.g.
`spack -d install py-scipy`) Spack is supposed to write a log of
compiler wrapper command line invocations to the current working
directory.

Due to a regression error introduced by #18205, these files were
no-longer generated, and Spack was printing errors such as
"No such file or directory: None/." This is because the log file
directory gets set from `spack.main.spack_working_dir`, but that
variable is not set in the spawned process.

This PR ensures that the working directory (at the time of the
"spack install" invocation) is persisted to the subprocess.

lib/spack/spack/subprocess_context.py
@@ -69,10 +69,12 @@ class PackageInstallContext(object):
             self.serialized_pkg = serialize(pkg)
         else:
             self.pkg = pkg
+        self.spack_working_dir = spack.main.spack_working_dir
         self.test_state = TestState()
 
     def restore(self):
         self.test_state.restore()
+        spack.main.spack_working_dir = self.spack_working_dir
         if _serialize:
             return pickle.load(self.serialized_pkg)
         else: