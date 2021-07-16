7cbb95ab45cb67dbc35df03588635b72221e0897
Marty Kandes <mkandes@sdsc.edu>
2021-07-15 17:06:19 -0700

    Add spades@3.15.2 % gcc@10.2.0; install after raxml-ng@1.0.2
    Also placed latest version of spades package from spack/spack develop
    branch in sdsc package repo and then modified it to include the latest
    versions of spades, which were not yet in the package. These
    modifications could be contributed back to the Spack project at some
    point in the future.

--- etc/spack/sdsc/expanse/0.15.4/cpu/specs/gcc@10.2.0/spades@3.15.2.sh
+++ etc/spack/sdsc/expanse/0.15.4/cpu/specs/gcc@10.2.0/spades@3.15.2.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#SBATCH --job-name=raxml-ng@1.0.2
+#SBATCH --job-name=spades@3.15.2
 #SBATCH --account=use300
 #SBATCH --partition=debug
 #SBATCH --nodes=1
@@ -34,10 +34,10 @@ module load "${SCHEDULER_MODULE}"
 module list
 . "${SPACK_INSTANCE_DIR}/share/spack/setup-env.sh"
 
-declare -xr SPACK_PACKAGE='raxml-ng@1.0.2'
+declare -xr SPACK_PACKAGE='spades@3.15.2'
 declare -xr SPACK_COMPILER='gcc@10.2.0'
-declare -xr SPACK_VARIANTS='~mpi'
-declare -xr SPACK_DEPENDENCIES=''
+declare -xr SPACK_VARIANTS=''
+declare -xr SPACK_DEPENDENCIES="^python@3.8.5/$(spack find --format '{hash:7}' python@3.8.5 % ${SPACK_COMPILER})"
 declare -xr SPACK_SPEC="${SPACK_PACKAGE} % ${SPACK_COMPILER} ${SPACK_VARIANTS} ${SPACK_DEPENDENCIES}"
 
 printenv
@@ -64,6 +64,6 @@ fi
 
 spack module lmod refresh --delete-tree -y
 
-sbatch --dependency="afterok:${SLURM_JOB_ID}" 'spades@3.15.2.sh'
+#sbatch --dependency="afterok:${SLURM_JOB_ID}" ''
 
 sleep 60
