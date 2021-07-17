3e46aa582b48b8d3cbf84971a030f30c633da3e1
Marty Kandes <mkandes@sdsc.edu>
2021-07-16 17:59:58 -0700

    Add jellyfish@2.2.7 % gcc@10.2.0; install after bismark@0.19.0

--- etc/spack/sdsc/expanse/0.15.4/cpu/specs/gcc@10.2.0/jellyfish@2.2.7.sh
+++ etc/spack/sdsc/expanse/0.15.4/cpu/specs/gcc@10.2.0/jellyfish@2.2.7.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#SBATCH --job-name=bismark@0.19.0
+#SBATCH --job-name=jellyfish@2.2.7
 #SBATCH --account=use300
 #SBATCH --partition=debug
 #SBATCH --nodes=1
@@ -34,12 +34,10 @@ module load "${SCHEDULER_MODULE}"
 module list
 . "${SPACK_INSTANCE_DIR}/share/spack/setup-env.sh"
 
-
-# ==> Error: Cannot depend on 'berkeley-db@18.1.40%gcc@10.2.0 cflags="-O2 -march=native" cxxflags="-O2 -march=native" fflags="-O2 -march=native"  arch=linux-centos8-zen2' twice
-declare -xr SPACK_PACKAGE='bismark@0.19.0'
+declare -xr SPACK_PACKAGE='jellyfish@2.2.7'
 declare -xr SPACK_COMPILER='gcc@10.2.0'
 declare -xr SPACK_VARIANTS=''
-declare -xr SPACK_DEPENDENCIES="^bowtie2@2.4.2/$(spack find --format '{hash:7}' bowtie2@2.4.2 % ${SPACK_COMPILER})"
+declare -xr SPACK_DEPENDENCIES=''
 declare -xr SPACK_SPEC="${SPACK_PACKAGE} % ${SPACK_COMPILER} ${SPACK_VARIANTS} ${SPACK_DEPENDENCIES}"
 
 printenv
@@ -66,6 +64,6 @@ fi
 
 spack module lmod refresh --delete-tree -y
 
-sbatch --dependency="afterok:${SLURM_JOB_ID}" 'jellyfish@2.2.7.sh'
+#sbatch --dependency="afterok:${SLURM_JOB_ID}" ''
 
 sleep 60
