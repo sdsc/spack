179a594f54fd3b6ca25837306896e3a71e61765e
Marty Kandes <mkandes@sdsc.edu>
2021-07-26 10:33:30 -0700

    Add gromacs@2019.6 % gcc@10.2.0; install after plumed@2.6.1
    Note, this spec relies on the external LAPACK variant (+lapack), unlike
    the current installations of GROMACS deployed on Expanse. Other GROMACS
    dependencies already relied on OpenBLAS as their BLAS/LAPACK library.
    The aim here with using OpenBLAS to satisfy the +lapack variant
    dependency is to eliminate the use of multiple versions of LAPACK within
    the same build of GROMACS.

--- etc/spack/sdsc/expanse/0.15.4/cpu/specs/gcc@10.2.0/openmpi@4.0.5/gromacs@2019.6.sh
+++ etc/spack/sdsc/expanse/0.15.4/cpu/specs/gcc@10.2.0/openmpi@4.0.5/gromacs@2019.6.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#SBATCH --job-name=plumed@2.6.1
+#SBATCH --job-name=gromacs@2019.6
 #SBATCH --account=use300
 #SBATCH --partition=debug
 #SBATCH --nodes=1
@@ -34,10 +34,10 @@ module load "${SCHEDULER_MODULE}"
 module list
 . "${SPACK_INSTANCE_DIR}/share/spack/setup-env.sh"
 
-declare -xr SPACK_PACKAGE='plumed@2.6.1'
+declare -xr SPACK_PACKAGE='gromacs@2019.6'
 declare -xr SPACK_COMPILER='gcc@10.2.0'
-declare -xr SPACK_VARIANTS='+gsl +mpi +shared'
-declare -xr SPACK_DEPENDENCIES="^openblas@0.3.10/$(spack find --format '{hash:7}' openblas@0.3.10 % ${SPACK_COMPILER} +ilp64 threads=none) ^openmpi@4.0.5/$(spack find --format '{hash:7}' openmpi@4.0.5 % ${SPACK_COMPILER}) ^python@3.8.5/$(spack find --format '{hash:7}' python@3.8.5 % ${SPACK_COMPILER})"
+declare -xr SPACK_VARIANTS='~cuda ~double ~double_precision +hwloc +lapack ~mdrun_only +mpi ~nosuffix ~opencl +openmp +plumed +shared'
+declare -xr SPACK_DEPENDENCIES="^fftw@3.3.8/$(spack find --format '{hash:7}' fftw@3.3.8 % ${SPACK_COMPILER} +mpi ^openmpi@4.0.5) ^python@3.8.5/$(spack find --format '{hash:7}' python@3.8.5 % ${SPACK_COMPILER}) ^openblas@0.3.10/$(spack find --format '{hash:7}' openblas@0.3.10 % ${SPACK_COMPILER} +ilp64 threads=none)"
 declare -xr SPACK_SPEC="${SPACK_PACKAGE} % ${SPACK_COMPILER} ${SPACK_VARIANTS} ${SPACK_DEPENDENCIES}"
 
 printenv
@@ -64,6 +64,6 @@ fi
 
 spack module lmod refresh --delete-tree -y
 
-sbatch --dependency="afterok:${SLURM_JOB_ID}" 'gromacs@2019.6.sh'
+#sbatch --dependency="afterok:${SLURM_JOB_ID}" ''
 
 sleep 60
