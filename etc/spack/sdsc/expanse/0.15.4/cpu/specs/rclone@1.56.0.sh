7d01207acf605411e83968e13d862f23d395d1aa
Marty Kandes <mkandes@sdsc.edu>
2021-09-02 18:53:23 -0700

    Add rclone@1.56.0 to core expanse/0.15.4/cpu utilities
    Installed after aria2@1.35.0 in core expanse/0.15.4/cpu utility
    dependency chain. The rclone package should be removed from the sdsc
    package repo when available in builtin repo on upgrade to Spack v0.17.x.

--- etc/spack/sdsc/expanse/0.15.4/cpu/specs/rclone@1.56.0.sh
+++ etc/spack/sdsc/expanse/0.15.4/cpu/specs/rclone@1.56.0.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#SBATCH --job-name=aria2@1.35.0
+#SBATCH --job-name=rclone@1.56.0
 #SBATCH --account=use300
 #SBATCH --partition=shared
 #SBATCH --nodes=1
@@ -34,7 +34,7 @@ module load "${SCHEDULER_MODULE}"
 module list
 . "${SPACK_INSTANCE_DIR}/share/spack/setup-env.sh"
 
-declare -xr SPACK_PACKAGE='aria2@1.35.0'
+declare -xr SPACK_PACKAGE='rclone@1.56.0'
 declare -xr SPACK_COMPILER='gcc@8.3.1'
 declare -xr SPACK_VARIANTS=''
 declare -xr SPACK_DEPENDENCIES=''
@@ -64,6 +64,6 @@ fi
 
 spack module lmod refresh --delete-tree -y
 
-sbatch --dependency="afterok:${SLURM_JOB_ID}" 'rclone@1.56.0.sh'
+#sbatch --dependency="afterok:${SLURM_JOB_ID}" ''
 
 sleep 60
