dc898b33cd02c59969fb8973106710455c234562
Marty Kandes <mkandes@sdsc.edu>
2021-10-04 12:26:13 -0700

    Add init pkgs to openmpi@4.0.5 % gcc@8.4.0 dep chain for expanse/0.15.4/gpu

---
+++ etc/spack/sdsc/expanse/0.15.4/gpu/specs/gcc@8.4.0/openmpi@4.0.5/namd@2.13.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+#SBATCH --job-name=namd@2.13
+#SBATCH --account=use300
+#SBATCH --partition=gpu-debug
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=93G
+#SBATCH --gpus=1
+#SBATCH --time=00:30:00
+#SBATCH --output=%x.o%j.%N
+
+declare -xr LOCAL_TIME="$(date +'%Y%m%dT%H%M%S%z')"
+declare -xir UNIX_TIME="$(date +'%s')"
+
+declare -xr SYSTEM_NAME='expanse'
+
+declare -xr SPACK_VERSION='0.15.4'
+declare -xr SPACK_INSTANCE_NAME='gpu'
+declare -xr SPACK_INSTANCE_DIR="${HOME}/cm/shared/apps/spack/${SPACK_VERSION}/${SPACK_INSTANCE_NAME}"
+
+declare -xr SLURM_JOB_SCRIPT="$(scontrol show job ${SLURM_JOB_ID} | awk -F= '/Command=/{print $2}')"
+declare -xr SLURM_JOB_MD5SUM="$(md5sum ${SLURM_JOB_SCRIPT})"
+
+declare -xr SCHEDULER_MODULE='slurm/expanse/20.02.3'
+
+echo "${UNIX_TIME} ${SLURM_JOB_ID} ${SLURM_JOB_MD5SUM} ${SLURM_JOB_DEPENDENCY}" 
+echo ""
+
+cat "${SLURM_JOB_SCRIPT}"
+
+module purge
+module load "${SCHEDULER_MODULE}"
+module list
+. "${SPACK_INSTANCE_DIR}/share/spack/setup-env.sh"
+
+declare -xr SPACK_PACKAGE='namd@2.13'
+declare -xr SPACK_COMPILER='gcc@8.4.0'
+declare -xr SPACK_VARIANTS='+cuda cuda_arch=70'
+declare -xr SPACK_DEPENDENCIES="^charmpp@6.8.2/$(spack find --format '{hash:7}' charmpp@6.8.2 % ${SPACK_COMPILER})"
+declare -xr SPACK_SPEC="${SPACK_PACKAGE} % ${SPACK_COMPILER} ${SPACK_VARIANTS} ${SPACK_DEPENDENCIES}"
+
+printenv
+
+spack config get compilers
+spack config get config  
+spack config get mirrors
+spack config get modules
+spack config get packages
+spack config get repos
+spack config get upstreams
+
+spack spec --long --namespaces --types namd@2.13 % gcc@8.4.0 +cuda cuda_arch=70 "^charmpp@6.8.2/$(spack find --format '{hash:7}' charmpp@6.8.2 % ${SPACK_COMPILER})"
+if [[ "${?}" -ne 0 ]]; then
+  echo 'ERROR: spack concretization failed.'
+  exit 1
+fi
+
+time -p spack install --jobs "${SLURM_CPUS_PER_TASK}" --fail-fast --yes-to-all namd@2.13 % gcc@8.4.0 +cuda cuda_arch=70 "^charmpp@6.8.2/$(spack find --format '{hash:7}' charmpp@6.8.2 % ${SPACK_COMPILER})"
+if [[ "${?}" -ne 0 ]]; then
+  echo 'ERROR: spack install failed.'
+  exit 1
+fi
+
+spack module lmod refresh --delete-tree -y
+
+sbatch --dependency="afterok:${SLURM_JOB_ID}" 'charmpp@6.10.2.sh'
+
+sleep 60
