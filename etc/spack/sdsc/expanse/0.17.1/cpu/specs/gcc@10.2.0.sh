0e9aa8fc0faf3ab10fbb79be3857bdc36dbb5b98
Marty Kandes <mkandes@sdsc.edu>
2022-01-29 18:10:48 -0800

    Update SPACK_VERSION in all core compiler and package specs to 0.17.1

--- etc/spack/sdsc/expanse/0.17.1/cpu/specs/gcc@10.2.0.sh
+++ etc/spack/sdsc/expanse/0.17.1/cpu/specs/gcc@10.2.0.sh
@@ -16,7 +16,7 @@ declare -xir UNIX_TIME="$(date +'%s')"
 
 declare -xr SYSTEM_NAME='expanse'
 
-declare -xr SPACK_VERSION='0.15.4'
+declare -xr SPACK_VERSION='0.17.1'
 declare -xr SPACK_INSTANCE_NAME='cpu'
 declare -xr SPACK_INSTANCE_DIR="${HOME}/cm/shared/apps/spack/${SPACK_VERSION}/${SPACK_INSTANCE_NAME}"
 
