62bfa26d8e5bf8f6c60eab9345d2cb9f6fd1bf98
Marty Kandes <mkandes@sdsc.edu>
2022-02-22 06:58:43 -0800

    Updated openmpi@4.1.1 % gcc@10.2.0 package dep chain for expanse/0.17.1/cpu
    Moving from openmpi@4.0.5 to openmpi@4.1.1 as part of the upgrade from
    spack v0.15.4 to v0.17.1. Again, in general, each package was updated to
    the latest 'preferred' spack version of the package available in v0.17.1.

---
+++ etc/spack/sdsc/expanse/0.17.1/cpu/specs/gcc@10.2.0/openmpi@4.1.1/trilinos@13.0.1.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+
+#SBATCH --job-name=trilinos@13.0.1
+#SBATCH --account=use300
+#SBATCH --partition=compute
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=32G
+#SBATCH --time=01:00:00
+#SBATCH --output=%x.o%j.%N
+
+declare -xr LOCAL_TIME="$(date +'%Y%m%dT%H%M%S%z')"
+declare -xir UNIX_TIME="$(date +'%s')"
+
+declare -xr SYSTEM_NAME='expanse'
+
+declare -xr SPACK_VERSION='0.15.4'
+declare -xr SPACK_INSTANCE_NAME='cpu'
+declare -xr SPACK_INSTANCE_DIR="${HOME}/cm/shared/apps/spack/${SPACK_VERSION}/${SPACK_INSTANCE_NAME}"
+
+declare -xr SLURM_JOB_SCRIPT="$(scontrol show job ${SLURM_JOB_ID} | awk -F= '/Command=/{print $2}')"
+declare -xr SLURM_JOB_MD5SUM="$(md5sum ${SLURM_JOB_SCRIPT})"
+
+declare -xr SCHEDULER_MODULE='slurm/expanse/20.02.3'
+
+echo "${UNIX_TIME} ${SLURM_JOB_ID} ${SLURM_JOB_MD5SUM} ${SLURM_JOB_DEPENDENCY}" 
+echo ""
+
+cat "${SLURM_JOB_SCRIPT}"
+
+module purge
+module load "${SCHEDULER_MODULE}"
+module list
+. "${SPACK_INSTANCE_DIR}/share/spack/setup-env.sh"
+
+
+# Trilinos can not be built against 64bit int hypre
+# depends_on('hypre~internal-superlu~int64', when='+hypre')
+#
+# ==> Error: Cannot depend on 'hwloc@1.11.11 twice
+declare -xr SPACK_PACKAGE='trilinos@13.0.1'
+declare -xr SPACK_COMPILER='gcc@10.2.0'
+declare -xr SPACK_VARIANTS='+adios2 +amesos +amesos2 +anasazi +aztec ~basker +belos +boost ~chaco ~complex ~cuda ~cuda_rdc ~debug ~dtk +epetra +epetraext ~epetraextbtf ~epetraextexperimental ~epetraextgraphreorderings ~exodus +explicit_template_instantiation ~float +fortran +gtest +hdf5 +hypre +ifpack +ifpack2 ~intrepid ~intrepid2 ~ipo ~isorropia +kokkos ~mesquite ~minitensor +ml +mpi +muelu +mumps ~nox ~openmp ~phalanx ~piro +python ~rol ~rythmos +sacado ~scorec ~shards +shared ~shylu ~stk ~stokhos ~stratimikos +strumpack +suite-sparse +superlu +superlu-dist ~teko ~tempus +tpetra ~trilinoscouplings ~wrapper ~x11 +zoltan +zoltan2'
+declare -xr SPACK_DEPENDENCIES="^boost@1.74.0/$(spack find --format '{hash:7}' boost@1.74.0 % ${SPACK_COMPILER} ~mpi) ^hdf5@1.10.7/$(spack find --format '{hash:7}' hdf5@1.10.7 % ${SPACK_COMPILER} +mpi ^openmpi@4.0.5) ^parmetis@4.0.3/$(spack find --format '{hash:7}' parmetis@4.0.3 % ${SPACK_COMPILER} ^openmpi@4.0.5) ^hwloc@1.11.11"
+#^hypre@2.19.0/$(spack find --format '{hash:7}' hypre@2.19.0 % ${SPACK_COMPILER} +int64 +mpi ^openmpi@4.0.5)"
+declare -xr SPACK_SPEC="${SPACK_PACKAGE} % ${SPACK_COMPILER} ${SPACK_VARIANTS} ${SPACK_DEPENDENCIES}"
+
+printenv
+
+spack config get compilers  
+spack config get config  
+spack config get mirrors
+spack config get modules
+spack config get packages
+spack config get repos
+spack config get upstreams
+
+spack spec --long --namespaces --types "${SPACK_SPEC}"
+if [[ "${?}" -ne 0 ]]; then
+  echo 'ERROR: spack concretization failed.'
+  exit 1
+fi
+
+time -p spack install --jobs "${SLURM_CPUS_PER_TASK}" --fail-fast --yes-to-all "${SPACK_SPEC}"
+if [[ "${?}" -ne 0 ]]; then
+  echo 'ERROR: spack install failed.'
+  exit 1
+fi
+
+spack module lmod refresh --delete-tree -y
+
+#sbatch --dependency="afterok:${SLURM_JOB_ID}" ''
+
+sleep 60
