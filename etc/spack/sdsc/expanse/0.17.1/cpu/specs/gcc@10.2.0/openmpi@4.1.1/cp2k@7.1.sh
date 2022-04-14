71796822ee569592a4d756cd16886f33ec824e70
Marty Kandes <mkandes@sdsc.edu>
2022-04-14 09:51:04 -0700

    Add quantum-espresso@7.0 % gcc@10.2.0 ^openmpi@4.1.1 to expanse/0.17.1/cpu

--- etc/spack/sdsc/expanse/0.17.1/cpu/specs/gcc@10.2.0/openmpi@4.1.1/cp2k@7.1.sh
+++ etc/spack/sdsc/expanse/0.17.1/cpu/specs/gcc@10.2.0/openmpi@4.1.1/cp2k@7.1.sh
@@ -191,6 +191,6 @@ fi
 
 spack module lmod refresh --delete-tree -y
 
-#sbatch --dependency="afterok:${SLURM_JOB_ID}" ''
+sbatch --dependency="afterok:${SLURM_JOB_ID}" 'quantum-espresso@7.0.sh'
 
 sleep 60
