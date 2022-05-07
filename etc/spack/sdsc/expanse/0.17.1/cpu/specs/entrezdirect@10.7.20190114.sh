6f5ebcc42a600d21d844b535ef36ff8aa26192a8
Marty Kandes <mkandes@sdsc.edu>
2022-05-07 11:58:38 -0700

    Update core compilers and packages for expanse/0.17.1/cpu

--- etc/spack/sdsc/expanse/0.17.1/cpu/specs/entrezdirect@10.7.20190114.sh
+++ etc/spack/sdsc/expanse/0.17.1/cpu/specs/entrezdirect@10.7.20190114.sh
@@ -22,7 +22,7 @@ declare -xr SPACK_INSTANCE_DIR="${HOME}/cm/shared/apps/spack/${SPACK_VERSION}/${
 declare -xr SLURM_JOB_SCRIPT="$(scontrol show job ${SLURM_JOB_ID} | awk -F= '/Command=/{print $2}')"
 declare -xr SLURM_JOB_MD5SUM="$(md5sum ${SLURM_JOB_SCRIPT})"
 
-declare -xr SCHEDULER_MODULE='slurm/expanse/current'
+declare -xr SCHEDULER_MODULE='slurm'
 
 echo "${UNIX_TIME} ${SLURM_JOB_ID} ${SLURM_JOB_MD5SUM} ${SLURM_JOB_DEPENDENCY}" 
 echo ""
@@ -64,6 +64,6 @@ fi
 
 spack module lmod refresh --delete-tree -y
 
-#sbatch --dependency="afterok:${SLURM_JOB_ID}" ''
+sbatch --dependency="afterok:${SLURM_JOB_ID}" 'anaconda3@2021.05.sh'
 
 sleep 60
