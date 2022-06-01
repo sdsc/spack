c98a9c5287007b0f29116a21b7c5a1a92a1b82eb
Marty Kandes <mkandes@sdsc.edu>
2022-05-31 17:11:20 -0700

    Remove ~profiled variant from gcc@10.2.0; not available for this gcc version

--- etc/spack/sdsc/expanse/0.18.0/cpu/specs/gcc@10.2.0.sh
+++ etc/spack/sdsc/expanse/0.18.0/cpu/specs/gcc@10.2.0.sh
@@ -36,7 +36,7 @@ module list
 
 declare -xr SPACK_PACKAGE='gcc@10.2.0'
 declare -xr SPACK_COMPILER='gcc@8.5.0'
-declare -xr SPACK_VARIANTS='~binutils ~bootstrap ~graphite ~nvptx ~piclibs ~profiled ~strip'
+declare -xr SPACK_VARIANTS='~binutils ~bootstrap ~graphite ~nvptx ~piclibs ~strip'
 declare -xr SPACK_DEPENDENCIES=''
 declare -xr SPACK_SPEC="${SPACK_PACKAGE} % ${SPACK_COMPILER} ${SPACK_VARIANTS} ${SPACK_DEPENDENCIES}"
 
