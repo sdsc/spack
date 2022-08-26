87167945fcf92b48006191b34676caa836aad46f
Marty Kandes <mkandes@sdsc.edu>
2022-08-26 09:19:25 -0700

    Use ucx@1.10.1 % gcc@8.5.0 as dep for all openmpi@4.1.3 installs

--- etc/spack/sdsc/expanse/0.17.2/cpu/specs/aocc@3.2.0/openmpi@4.1.3.sh
+++ etc/spack/sdsc/expanse/0.17.2/cpu/specs/aocc@3.2.0/openmpi@4.1.3.sh
@@ -34,39 +34,10 @@ module load "${SCHEDULER_MODULE}"
 module list
 . "${SPACK_INSTANCE_DIR}/share/spack/setup-env.sh"
 
-# when attempting to build all dependencies natively with aocc@3.2.0; must build some dependencies with gcc as outlined in AMD instructions; see https://developer.amd.com/spack/hpc-applications-openmpi/ 
-#
-# ==> Installing ucx-1.10.1-3dp52porla3vtarg5ez75ukfl7i7d23s
-# ==> No binary for ucx-1.10.1-3dp52porla3vtarg5ez75ukfl7i7d23s found: installing from source
-# ==> Using cached archive: /home/mkandes/cm/shared/apps/spack/0.17.2/cpu/var/spack/cache/_source-cache/archive/ae/ae9a108af6842ca135e7ec9b6131469adf9f1e50f899349fafcc69a215368bc9.tar.gz
-# ==> No patches needed for ucx
-# ==> ucx: Executing phase: 'autoreconf'
-# ==> ucx: Executing phase: 'configure'
-# ==> Error: ProcessError: Command exited with status 127:
-#    'contrib/configure-release' '--prefix=/home/mkandes/cm/shared/apps/spack/0.17.2/cpu/opt/spack/linux-rocky8-zen2/aocc-3.2.0/ucx-1.10.1-3dp52porla3vtarg5ez75ukfl7i7d23s' '--enable-mt' '--disable-cma' '--disable-params-check' '--with-avx' '--enable-optimizations' '--disable-assertions' '--disable-logging' '--with-pic' '--without-rc' '--without-ud' '--without-dc' '--without-mlx5-dv' '--without-ib-hw-tm' '--without-dm' '--without-cm' '--without-rocm' '--without-java' '--without-cuda' '--without-gdrcopy' '--without-knem' '--without-xpmem' 'LDFLAGS=-fuse-ld=bfd'
-#
-# 1 error found in build log:
-#     287    checking whether bfd_get_section_flags is declared... no
-#     288    checking whether bfd_section_flags is declared... no
-#     289    checking whether bfd_get_section_vma is declared... no
-#     290    checking whether bfd_section_vma is declared... no
-#     291    checking bfd_section_size API version... 2-args API
-#     292    configure: WARNING: detailed backtrace is not supported
-#  >> 293    checking __attribute__((constructor))... configure: error: Cannot c
-#            ontinue. Please use compiler that
-#     294                                 supports __attribute__((constructor))
-#
-#See build log for details:
-#  /tmp/mkandes/spack-stage/spack-stage-ucx-1.10.1-3dp52porla3vtarg5ez75ukfl7i7d23s/spack-build-out.txt
-#
-#==> Warning: Skipping build of openmpi-4.1.3-ikwulb2tnxsgpfl5sf7rutwgk7dpulak since ucx-1.10.1-3dp52porla3vtarg5ez75ukfl7i7d23s failed
-#==> Error: Terminating after first install failure: ProcessError: Command exited with status 127:
-#    'contrib/configure-release' '--prefix=/home/mkandes/cm/shared/apps/spack/0.17.2/cpu/opt/spack/linux-rocky8-zen2/aocc-3.2.0/ucx-1.10.1-3dp52porla3vtarg5ez75ukfl7i7d23s' '--enable-mt' '--disable-cma' '--disable-params-check' '--with-avx' '--enable-optimizations' '--disable-assertions' '--disable-logging' '--with-pic' '--without-rc' '--without-ud' '--without-dc' '--without-mlx5-dv' '--without-ib-hw-tm' '--without-dm' '--without-cm' '--without-rocm' '--without-java' '--without-cuda' '--without-gdrcopy' '--without-knem' '--without-xpmem' 'LDFLAGS=-fuse-ld=bfd'
-
 declare -xr SPACK_PACKAGE='openmpi@4.1.3'
 declare -xr SPACK_COMPILER='aocc@3.2.0'
-declare -xr SPACK_VARIANTS='~atomics~cuda~cxx~cxx_exceptions~gpfs~internal-hwloc~java+legacylaunchers+lustre~memchecker+pmi+pmix+romio~rsh~singularity+static+vt+wrapper-rpath cuda_arch=none fabrics=ucx,xpmem,knem,hcoll schedulers=slurm'
-declare -xr SPACK_DEPENDENCIES="^lustre@2.12.8 ^slurm@21.08.8 ^ucx@1.10.1/$(spack find --format '{hash:7}' ucx@1.10.1 % gcc@8.5.0)"
+declare -xr SPACK_VARIANTS='~atomics~cuda~cxx~cxx_exceptions~gpfs~internal-hwloc~java+legacylaunchers+lustre~memchecker+pmi+pmix+romio~rsh~singularity+static+vt+wrapper-rpath cuda_arch=none fabrics=ucx schedulers=slurm'
+declare -xr SPACK_DEPENDENCIES="^lustre@2.12.8 ^slurm@21.08.8 ^rdma-core@28.0 ^ucx@1.10.1/$(spack find --format '{hash:7}' ucx@1.10.1 % gcc@8.5.0)"
 declare -xr SPACK_SPEC="${SPACK_PACKAGE} % ${SPACK_COMPILER} ${SPACK_VARIANTS} ${SPACK_DEPENDENCIES}"
 
 printenv
@@ -80,13 +51,13 @@ spack config get repos
 spack config get upstreams
 
 echo "${SPACK_SPEC}"
-spack spec --long --namespaces --types openmpi@4.1.3 % aocc@3.2.0 ~atomics~cuda~cxx~cxx_exceptions~gpfs~internal-hwloc~java+legacylaunchers+lustre~memchecker+pmi+pmix+romio~rsh~singularity+static+vt+wrapper-rpath cuda_arch=none fabrics=ucx schedulers=slurm ^lustre@2.12.8 ^slurm@21.08.8 "^ucx@1.10.1/$(spack find --format '{hash:7}' ucx@1.10.1 % gcc@8.5.0)"
+spack spec --long --namespaces --types openmpi@4.1.3 % aocc@3.2.0 ~atomics~cuda~cxx~cxx_exceptions~gpfs~internal-hwloc~java+legacylaunchers+lustre~memchecker+pmi+pmix+romio~rsh~singularity+static+vt+wrapper-rpath cuda_arch=none fabrics=ucx schedulers=slurm ^lustre@2.12.8 ^slurm@21.08.8 ^rdma-core@28.0 "^ucx@1.10.1/$(spack find --format '{hash:7}' ucx@1.10.1 % gcc@8.5.0)"
 if [[ "${?}" -ne 0 ]]; then
   echo 'ERROR: spack concretization failed.'
   exit 1
 fi
 
-time -p spack install --jobs "${SLURM_CPUS_PER_TASK}" --fail-fast --yes-to-all openmpi@4.1.3 % aocc@3.2.0 ~atomics~cuda~cxx~cxx_exceptions~gpfs~internal-hwloc~java+legacylaunchers+lustre~memchecker+pmi+pmix+romio~rsh~singularity+static+vt+wrapper-rpath cuda_arch=none fabrics=ucx schedulers=slurm ^lustre@2.12.8 ^slurm@21.08.8 "^ucx@1.10.1/$(spack find --format '{hash:7}' ucx@1.10.1 % gcc@8.5.0)"
+time -p spack install --jobs "${SLURM_CPUS_PER_TASK}" --fail-fast --yes-to-all openmpi@4.1.3 % aocc@3.2.0 ~atomics~cuda~cxx~cxx_exceptions~gpfs~internal-hwloc~java+legacylaunchers+lustre~memchecker+pmi+pmix+romio~rsh~singularity+static+vt+wrapper-rpath cuda_arch=none fabrics=ucx schedulers=slurm ^lustre@2.12.8 ^slurm@21.08.8 ^rdma-core@28.0 "^ucx@1.10.1/$(spack find --format '{hash:7}' ucx@1.10.1 % gcc@8.5.0)"
 if [[ "${?}" -ne 0 ]]; then
   echo 'ERROR: spack install failed.'
   exit 1
--- etc/spack/sdsc/expanse/0.17.2/cpu/specs/ucx@1.10.1.sh
+++ etc/spack/sdsc/expanse/0.17.2/cpu/specs/ucx@1.10.1.sh
@@ -34,44 +34,13 @@ module load "${SCHEDULER_MODULE}"
 module list
 . "${SPACK_INSTANCE_DIR}/share/spack/setup-env.sh"
 
+
 # Note from AMD: +cm option may not work with RHEL 8 and later. 
 # https://developer.amd.com/spack/hpc-applications-openmpi/
-#
-# ==> Installing ucx-1.10.1-p3zsepkh3zwlw2mdsc3zajgxfgoqblho
-# ==> No binary for ucx-1.10.1-p3zsepkh3zwlw2mdsc3zajgxfgoqblho found: installing from source
-# ==> Using cached archive: /home/mkandes/cm/shared/apps/spack/0.17.2/cpu/var/spack/cache/_source-cache/archive/ae/ae9a108af6842ca135e7ec9b6131469adf9f1e50f899349fafcc
-# 69a215368bc9.tar.gz
-# ==> No patches needed for ucx
-# ==> ucx: Executing phase: 'autoreconf'
-# ==> ucx: Executing phase: 'configure'
-# ==> Error: ProcessError: Command exited with status 1:
-#    'contrib/configure-release' '--prefix=/home/mkandes/cm/shared/apps/spack/0.17.2/cpu/opt/spack/linux-rocky8-zen/gcc-8.5.0/ucx-1.10.1-p3zsepkh3zwlw2mdsc3zajgxfgoqb
-# lho' '--enable-mt' '--enable-cma' '--disable-params-check' '--with-avx' '--enable-optimizations' '--disable-assertions' '--disable-logging' '--with-pic' '--with-rc' 
-# '--with-ud' '--with-dc' '--with-mlx5-dv' '--with-ib-hw-tm' '--with-dm' '--with-cm' '--without-rocm' '--without-java' '--without-cuda' '--without-gdrcopy' '--with-kne
-# m=/home/mkandes/cm/shared/apps/spack/0.17.2/cpu/opt/spack/linux-rocky8-zen/gcc-8.5.0/knem-1.1.4-dtpqb42ih64kccsdfnq75xnlxjun2zps' '--with-xpmem=/home/mkandes/cm/shar
-# ed/apps/spack/0.17.2/cpu/opt/spack/linux-rocky8-zen/gcc-8.5.0/xpmem-2.6.5-36-jnrkrz24hnkz4sdli7smyv5enp27t5kw'
-#
-#1 error found in build log:
-#     399    checking for struct ibv_tm_caps.flags... yes
-#     400    checking whether ibv_exp_alloc_dm is declared... no
-#     401    checking whether ibv_alloc_dm is declared... yes
-#     402    checking whether ibv_cmd_modify_qp is declared... no
-#     403    configure: Checking OFED valgrind libs /usr/lib64/mlnx_ofed/valgrin
-#            d
-#     404    checking for ib_cm_send_req in -libcm... no
-#  >> 405    configure: error: CM requested but lib ibcm not found
-#
-#See build log for details:
-#  /tmp/mkandes/spack-stage/spack-stage-ucx-1.10.1-p3zsepkh3zwlw2mdsc3zajgxfgoqblho/spack-build-out.txt
-#
-# ==> Error: Terminating after first install failure: ProcessError: Command exited with status 1:
-#    'contrib/configure-release' '--prefix=/home/mkandes/cm/shared/apps/spack/0.17.2/cpu/opt/spack/linux-rocky8-zen/gcc-8.5.0/ucx-1.10.1-p3zsepkh3zwlw2mdsc3zajgxfgoqblho' '--enable-mt' '--enable-cma' '--disable-params-check' '--with-avx' '--enable-optimizations' '--disable-assertions' '--disable-logging' '--with-pic' '--with-rc' '--with-ud' '--with-dc' '--with-mlx5-dv' '--with-ib-hw-tm' '--with-dm' '--with-cm' '--without-rocm' '--without-java' '--without-cuda' '--without-gdrcopy' '--with-knem=/home/mkandes/cm/shared/apps/spack/0.17.2/cpu/opt/spack/linux-rocky8-zen/gcc-8.5.0/knem-1.1.4-dtpqb42ih64kccsdfnq75xnlxjun2zps' '--with-xpmem=/home/mkandes/cm/shared/apps/spack/0.17.2/cpu/opt/spack/linux-rocky8-zen/gcc-8.5.0/xpmem-2.6.5-36-jnrkrz24hnkz4sdli7smyv5enp27t5kw'
 
 declare -xr SPACK_PACKAGE='ucx@1.10.1'
 declare -xr SPACK_COMPILER='gcc@8.5.0'
-#declare -xr SPACK_VARIANTS='~assertions ~cm +cma ~cuda +dc ~debug +dm ~gdrcopy +ib-hw-tm ~java +knem ~logging +mlx5-dv +optimizations ~parameter_checking +pic +rc ~rocm +thread_multiple +ud +xpmem'
-declare -xr SPACK_VARIANTS='~assertions ~cm ~cma ~cuda ~dc ~debug ~dm ~gdrcopy ~ib-hw-tm ~java ~knem ~logging ~mlx5-dv +optimizations ~parameter_checking +pic ~rc ~rocm +thread_multiple ~ud ~xpmem'
-#declare -xr SPACK_DEPENDENCIES="^xpmem@2.6.5-36/$(spack find --format '{hash:7}' xpmem@2.6.5-36 % ${SPACK_COMPILER}) ^hwloc@2.6.0 +libudev"
+declare -xr SPACK_VARIANTS='~assertions ~cm +cma ~cuda +dc ~debug +dm ~gdrcopy +ib-hw-tm ~java ~knem ~logging +mlx5-dv +optimizations ~parameter_checking +pic +rc ~rocm +thread_multiple +ud ~xpmem'
 declare -xr SPACK_DEPENDENCIES=''
 declare -xr SPACK_SPEC="${SPACK_PACKAGE} % ${SPACK_COMPILER} ${SPACK_VARIANTS} ${SPACK_DEPENDENCIES}"
 
@@ -98,7 +67,5 @@ if [[ "${?}" -ne 0 ]]; then
 fi
 
 spack module lmod refresh --delete-tree -y
-
-#sbatch --dependency="afterok:${SLURM_JOB_ID}" ''
 
 sleep 60
