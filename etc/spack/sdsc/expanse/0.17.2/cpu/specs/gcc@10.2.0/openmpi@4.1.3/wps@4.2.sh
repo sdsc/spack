6d609c585c7cf9515250203f024623c3fb570213
Marty Kandes <mkandes@sdsc.edu>
2022-08-26 09:51:25 -0700

    Add wrf@4.2 + wps@4.2 to openmpi@4.1.3 % gcc@10.2.0 dep chain

--- etc/spack/sdsc/expanse/0.17.2/cpu/specs/gcc@10.2.0/openmpi@4.1.3/wps@4.2.sh
+++ etc/spack/sdsc/expanse/0.17.2/cpu/specs/gcc@10.2.0/openmpi@4.1.3/wps@4.2.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#SBATCH --job-name=openfoam@2106
+#SBATCH --job-name=wps@4.2
 #SBATCH --account=use300
 #SBATCH --partition=shared
 #SBATCH --nodes=1
@@ -34,10 +34,10 @@ module load "${SCHEDULER_MODULE}"
 module list
 . "${SPACK_INSTANCE_DIR}/share/spack/setup-env.sh"
 
-declare -xr SPACK_PACKAGE='openfoam@2106'
+declare -xr SPACK_PACKAGE='wps@4.2'
 declare -xr SPACK_COMPILER='gcc@10.2.0'
-declare -xr SPACK_VARIANTS='~float32 +int64 ~kahip ~knl +metis +mgridgen +paraview +scotch +source ~spdp +vtk ~zoltan'
-declare -xr SPACK_DEPENDENCIES="^boost@1.77.0/$(spack find --format '{hash:7}' boost@1.77.0 % ${SPACK_COMPILER} ~mpi) ^cgal@4.13/$(spack find --format '{hash:7}' cgal@4.13 % ${SPACK_COMPILER}) ^fftw@3.3.10/$(spack find --format '{hash:7}' fftw@3.3.10 % ${SPACK_COMPILER} ~mpi ~openmp) ^metis@5.1.0/$(spack find --format '{hash:7}' metis@5.1.0 % ${SPACK_COMPILER} +int64 +real64) ^paraview@5.9.1/$(spack find --format '{hash:7}' paraview@5.9.1 % ${SPACK_COMPILER} +mpi ^openmpi@4.1.3) ^scotch@6.1.1/$(spack find --format '{hash:7}' scotch@6.1.1 % ${SPACK_COMPILER} +int64 +mpi ^openmpi@4.1.3)"
+declare -xr SPACK_VARIANTS='build_type=dmpar'
+declare -xr SPACK_DEPENDENCIES="^wrf@4.2/$(spack find --format '{hash:7}' wrf@4.2 % ${SPACK_COMPILER} ^openmpi@4.1.3)"
 declare -xr SPACK_SPEC="${SPACK_PACKAGE} % ${SPACK_COMPILER} ${SPACK_VARIANTS} ${SPACK_DEPENDENCIES}"
 
 printenv
@@ -50,13 +50,13 @@ spack config get packages
 spack config get repos
 spack config get upstreams
 
-spack --show-cores=minimized spec --long --namespaces --types "${SPACK_SPEC}"
+spack spec --long --namespaces --type wps@4.2 % gcc@10.2.0 build_type='dmpar' "^wrf@4.2/$(spack find --format '{hash:7}' wrf@4.2 % ${SPACK_COMPILER} ^openmpi@4.1.3)" 
 if [[ "${?}" -ne 0 ]]; then
   echo 'ERROR: spack concretization failed.'
   exit 1
 fi
 
-time -p spack install --jobs "${SLURM_CPUS_PER_TASK}" --fail-fast --yes-to-all "${SPACK_SPEC}"
+time -p spack install --jobs "${SLURM_CPUS_PER_TASK}" --fail-fast --yes-to-all wps@4.2 % gcc@10.2.0 build_type='dmpar' "^wrf@4.2/$(spack find --format '{hash:7}' wrf@4.2 % ${SPACK_COMPILER} ^openmpi@4.1.3)"
 if [[ "${?}" -ne 0 ]]; then
   echo 'ERROR: spack install failed.'
   exit 1
@@ -64,6 +64,6 @@ fi
 
 spack module lmod refresh --delete-tree -y
 
-sbatch --dependency="afterok:${SLURM_JOB_ID}" 'wrf@4.2.sh'
+#sbatch --dependency="afterok:${SLURM_JOB_ID}" ''
 
 sleep 60
