17ffaaf4f16ad32d9e9f18134a0e112789697ac5
Marty Kandes <mkandes@sdsc.edu>
2023-02-09 14:01:01 -0800

    Update SPECS.md for both expanse/0.17.3/cpu and expanse/0.17.3/gpu

--- etc/spack/sdsc/expanse/0.17.3/gpu/specs/gcc@10.2.0/openmpi@3.1.6/sprng@5.0.sh
+++ etc/spack/sdsc/expanse/0.17.3/gpu/specs/gcc@10.2.0/openmpi@3.1.6/sprng@5.0.sh
@@ -71,6 +71,6 @@ fi
 
 spack module lmod refresh --delete-tree -y
 
-sbatch --dependency="afterok:${SLURM_JOB_ID}" 'netlib-scalapack@2.1.0.sh'
+#sbatch --dependency="afterok:${SLURM_JOB_ID}" ''
 
 sleep 60
