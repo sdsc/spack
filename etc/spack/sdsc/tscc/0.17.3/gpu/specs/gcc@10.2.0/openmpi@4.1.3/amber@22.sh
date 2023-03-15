eb3c91f3b148c91067a5b885d76790bef21d5701
Jerry Greenberg <jpg@sdsc.edu>
2023-03-15 12:00:59 -0700

    initial checkin of gpu submit scripts

---
+++ etc/spack/sdsc/tscc/0.17.3/gpu/specs/gcc@10.2.0/openmpi@4.1.3/amber@22.sh
@@ -0,0 +1,104 @@
+#!/usr/bin/env bash
+
+#SBATCH --job-name=amber@22
+#SBATCH --account=use300
+##SBATCH --reservation=root_63
+#SBATCH --partition=ind-gpu-shared
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=93G
+#SBATCH --gpus=1
+#SBATCH --time=01:00:00
+#SBATCH --output=%x.o%j.%N
+
+declare -xr LOCAL_TIME="$(date +'%Y%m%dT%H%M%S%z')"
+declare -xir UNIX_TIME="$(date +'%s')"
+
+declare -xr SYSTEM_NAME='expanse'
+
+declare -xr SPACK_VERSION='0.17.3'
+declare -xr SPACK_INSTANCE_NAME='gpu'
+declare -xr SPACK_INSTANCE_DIR="${HOME}/cm/shared/apps/spack/${SPACK_VERSION}/${SPACK_INSTANCE_NAME}"
+
+declare -xr SLURM_JOB_SCRIPT="$(scontrol show job ${SLURM_JOB_ID} | awk -F= '/Command=/{print $2}')"
+declare -xr SLURM_JOB_MD5SUM="$(md5sum ${SLURM_JOB_SCRIPT})"
+
+declare -xr SCHEDULER_MODULE='slurm'
+declare -xr COMPILER_MODULE='gcc/10.2.0'
+declare -xr MPI_MODULE='openmpi/4.1.3'
+declare -xr CUDA_MODULE='cuda/11.2.2'
+declare -xr CMAKE_MODULE='cmake/3.21.4'
+
+echo "${UNIX_TIME} ${SLURM_JOB_ID} ${SLURM_JOB_MD5SUM} ${SLURM_JOB_DEPENDENCY}" 
+echo ""
+
+cat "${SLURM_JOB_SCRIPT}"
+
+module purge
+module load "${SCHEDULER_MODULE}"
+. "${SPACK_INSTANCE_DIR}/share/spack/setup-env.sh"
+module use "${SPACK_ROOT}/share/spack/lmod/linux-rocky8-x86_64/Core"
+module load "${COMPILER_MODULE}"
+module load "${MPI_MODULE}"
+module load "${CUDA_MODULE}"
+module load "${CMAKE_MODULE}"
+module list
+
+# 49387    [ 27%] Building CXX object AmberTools/src/cpptraj/src/CMakeFiles/
+#              cpptraj_common_obj.dir/Action_Box.cpp.o
+#  >> 49388    /home/mkandes/cm/shared/apps/spack/0.17.3/gpu/opt/spack/linux-roc
+#              ky8-cascadelake/gcc-10.2.0/cuda-11.2.2-blza2psofa3wr2zumqrnh4je2f
+#              7ze3mx/include/thrust/detail/allocator/allocator_traits.h(245): e
+#              rror: class "thrust::detail::device_delete_allocator" has no memb
+#              er "value_type"
+#     49389              detected during:
+#     49390                instantiation of class "thrust::detail::allocator_tra
+#              its<Alloc> [with Alloc=thrust::detail::device_delete_allocator]"
+#     49391    (402): here
+#     49392                instantiation of class "thrust::detail::allocator_sys
+#              tem<Alloc> [with Alloc=thrust::detail::device_delete_allocator]"
+#     49393    /home/mkandes/cm/shared/apps/spack/0.17.3/gpu/opt/spack/linux-roc
+#              ky8-cascadelake/gcc-10.2.0/cuda-11.2.2-blza2psofa3wr2zumqrnh4je2f
+#              7ze3mx/include/thrust/detail/allocator/destroy_range.inl(137): he
+#              re
+#
+# Fix? Use cuda/10.2.89?
+#
+# Currently Amber supports CUDA versions from 7.5 to 11.x inclusive (tested only up to 11.2). However, older
+# versions are less well tested and more likely to cause issues, and you may also run into trouble with the CUDA
+# SDK being incompatible with newer compilers on your machine.
+
+declare -xr SPACK_PACKAGE='amber@22'
+declare -xr SPACK_COMPILER='gcc@10.2.0'
+declare -xr SPACK_VARIANTS='+cuda cuda_arch=70,80 +mpi +openmp +update'
+declare -xr SPACK_DEPENDENCIES="^boost@1.77.0/$(spack find --format '{hash:7}' boost@1.77.0 % ${SPACK_COMPILER} ~mpi) ^openmpi@4.1.3/$(spack find --format '{hash:7}' openmpi@4.1.3 % ${SPACK_COMPILER}) ^netcdf-c@4.8.1/$(spack find --format '{hash:7}' netcdf-c@4.8.1 % ${SPACK_COMPILER} +mpi ^openmpi@4.1.3)"
+declare -xr SPACK_SPEC="${SPACK_PACKAGE} % ${SPACK_COMPILER} ${SPACK_VARIANTS} ${SPACK_DEPENDENCIES}"
+
+printenv
+
+spack config get compilers
+spack config get config  
+spack config get mirrors
+spack config get modules
+spack config get packages
+spack config get repos
+spack config get upstreams
+
+spack spec --long --namespaces --types amber@22 % gcc@10.2.0 +cuda cuda_arch=70,80 +mpi +openmp +update "${SPACK_DEPENDENCIES}"
+if [[ "${?}" -ne 0 ]]; then
+  echo 'ERROR: spack concretization failed.'
+  exit 1
+fi
+
+time -p spack install --jobs "${SLURM_CPUS_PER_TASK}" --fail-fast --yes-to-all amber@22 % gcc@10.2.0 +cuda cuda_arch=70,80 +mpi +openmp +update "${SPACK_DEPENDENCIES}"
+if [[ "${?}" -ne 0 ]]; then
+  echo 'ERROR: spack install failed.'
+  exit 1
+fi
+
+spack module lmod refresh --delete-tree -y
+
+#sbatch --dependency="afterok:${SLURM_JOB_ID}" ''
+
+sleep 60
---
+++ etc/spack/sdsc/tscc/0.17.3/gpu/specs/gcc@10.2.0/openmpi@4.1.3/paraview@5.9.1.sh
@@ -0,0 +1,174 @@
+#!/usr/bin/env bash
+
+#SBATCH --job-name=paraview@5.9.1
+#SBATCH --account=use300
+##SBATCH --reservation=root_63
+#SBATCH --partition=gpu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=40
+#SBATCH --mem=368G
+#SBATCH --gpus=4
+#SBATCH --time=48:00:00
+#SBATCH --output=%x.o%j.%N
+
+declare -xr LOCAL_TIME="$(date +'%Y%m%dT%H%M%S%z')"
+declare -xir UNIX_TIME="$(date +'%s')"
+
+declare -xr SYSTEM_NAME='expanse'
+
+declare -xr SPACK_VERSION='0.17.3'
+declare -xr SPACK_INSTANCE_NAME='gpu'
+declare -xr SPACK_INSTANCE_DIR="${HOME}/cm/shared/apps/spack/${SPACK_VERSION}/${SPACK_INSTANCE_NAME}"
+
+declare -xr SLURM_JOB_SCRIPT="$(scontrol show job ${SLURM_JOB_ID} | awk -F= '/Command=/{print $2}')"
+declare -xr SLURM_JOB_MD5SUM="$(md5sum ${SLURM_JOB_SCRIPT})"
+
+declare -xr SCHEDULER_MODULE='slurm'
+
+echo "${UNIX_TIME} ${SLURM_JOB_ID} ${SLURM_JOB_MD5SUM} ${SLURM_JOB_DEPENDENCY}" 
+echo ""
+
+cat "${SLURM_JOB_SCRIPT}"
+
+module purge
+module load "${SCHEDULER_MODULE}"
+module list
+. "${SPACK_INSTANCE_DIR}/share/spack/setup-env.sh"
+
+# 1 error found in build log:
+#     182    --  conduit::float64 native type: double
+#     183    -- Detecting Fortran/C Interface
+#     184    -- Detecting Fortran/C Interface - Found GLOBAL and MODULE mangling
+#     185    -- Found Python3: /home/mkandes/cm/shared/apps/spack/0.17.3/cpu/opt
+#            /spack/linux-rocky8-zen2/gcc-10.2.0/python-3.8.12-dtdsuje4z5v7rqevi
+#            nukk4n37p3lxjcn/include/python3.8 (found version "3.8.12") found co
+#            mponents: Development Development.Module Development.Embed
+#     186    -- Enabled modules: VTK(51), ParaView(20 + 3)
+#     187    -- Configuring done
+#  >> 188    CMake Error: The inter-target dependency graph contains the followi
+#            ng strongly connected component (cycle):
+#     189      "Parallel" of type SHARED_LIBRARY
+#     190        depends on "ParallelMPI-objects" (weak)
+#     191        depends on "ParallelCore-objects" (weak)
+#     192        depends on "IO" (weak)
+#     193        depends on "IOParallelXML-objects" (weak)
+#     194        depends on "FiltersExtraction-objects" (weak)
+#
+#See build log for details:
+#  /tmp/mkandes/spack-stage/spack-stage-paraview-5.9.1-nvcndznztjdzufmpkor2bqb6ffzftim7/spack-build-out.txt
+
+# CMake Error: The inter-target dependency graph contains the following strongly connected component (cycle):
+#  "Parallel" of type SHARED_LIBRARY
+#    depends on "ParallelMPI-objects" (weak)
+#    depends on "ParallelCore-objects" (weak)
+#    depends on "IO" (weak)
+#    depends on "IOParallelXML-objects" (weak)
+#    depends on "FiltersExtraction-objects" (weak)
+#    depends on "ParallelDIY-objects" (weak)
+#  "IO" of type SHARED_LIBRARY
+#    depends on "IOADIOS2-objects" (weak)
+#    depends on "ParallelCore-objects" (weak)
+#    depends on "Parallel" (weak)
+#    depends on "ParallelMPI-objects" (weak)
+#  "ParallelCore-objects" of type OBJECT_LIBRARY
+#    depends on "IO" (weak)
+#  "ParallelMPI-objects" of type OBJECT_LIBRARY
+#    depends on "ParallelCore-objects" (weak)
+#    depends on "IO" (weak)
+#  "IOParallelXML-objects" of type OBJECT_LIBRARY
+#    depends on "IO" (weak)
+#    depends on "ParallelCore-objects" (weak)
+#  "IOADIOS2-objects" of type OBJECT_LIBRARY
+#    depends on "ParallelCore-objects" (weak)
+#    depends on "IO" (weak)
+#    depends on "Parallel" (weak)
+#    depends on "ParallelMPI-objects" (weak)
+#  "ParallelDIY-objects" of type OBJECT_LIBRARY
+#    depends on "ParallelCore-objects" (weak)
+#    depends on "IO" (weak)
+#    depends on "ParallelMPI-objects" (weak)
+#  "FiltersExtraction-objects" of type OBJECT_LIBRARY
+#    depends on "ParallelDIY-objects" (weak)
+#    depends on "ParallelCore-objects" (weak)
+#    depends on "IO" (weak)
+#    depends on "ParallelMPI-objects" (weak)
+#At least one of these targets is not a STATIC_LIBRARY.  Cyclic dependencies are allowed only among static libraries.
+#CMake Generate step failed.  Build files cannot be regenerated correctly.
+
+# adios2 needs to be ~shared?
+
+# A conflict was triggered
+#  condition(3642)
+#  condition(3655)
+#  condition(3801)
+#  condition(3802)
+#  conflict("paraview",3801,3802)
+#  root("paraview")
+#  variant_condition(3642,"paraview","cuda")
+#  variant_condition(3655,"paraview","shared")
+#  variant_set("paraview","cuda","True")
+#  variant_set("paraview","shared","True")
+
+# 1 error found in build log:
+#     182    --  conduit::float32 native type: float
+#     183    --  conduit::float64 native type: double
+#     184    -- Detecting Fortran/C Interface
+#     185    -- Detecting Fortran/C Interface - Found GLOBAL and MODULE mangling
+#     186    -- Enabled modules: VTK(50), ParaView(20 + 3)
+#     187    -- Configuring done
+#  >> 188    CMake Error: The inter-target dependency graph contains the followi
+#            ng strongly connected component (cycle):
+#     189      "vtkPVInSituPython" of type STATIC_LIBRARY
+#     190        depends on "InSitu-objects" (weak)
+#     191        depends on "PythonCatalyst-objects" (weak)
+#     192        depends on "PythonInitializer" (weak)
+#     193        depends on "vtkPVCatalystPython" (weak)
+#     194        depends on "ClientKit" (weak)
+
+# A conflict was triggered
+#  condition(3650)
+#  condition(3803)
+#  condition(3804)
+#  conflict("paraview",3803,3804)
+#  no version satisfies the given constraints
+#  root("paraview")
+#  variant_condition(3650,"paraview","opengl2")
+#  variant_set("paraview","opengl2","False")
+#  version_satisfies("paraview","5.5:","5.9.1")
+#  version_satisfies("paraview","5.9.1")
+
+declare -xr SPACK_PACKAGE='paraview@5.9.1'
+declare -xr SPACK_COMPILER='gcc@10.2.0'
+declare -xr SPACK_VARIANTS='~adios2 ~advanced_debug +cuda cuda_arch=70 ~development_files +examples +hdf5 ~ipo ~kits +mpi +opengl2 ~osmesa ~python +python3 ~qt ~shared'
+declare -xr SPACK_DEPENDENCIES="^hdf5@1.10.7/$(spack find --format '{hash:7}' hdf5@1.10.7 % ${SPACK_COMPILER} +mpi ^openmpi@4.1.3) ^py-matplotlib@3.4.3/$(spack find --format '{hash:7}' py-matplotlib@3.4.3 % ${SPACK_COMPILER}) ^netcdf-c@4.8.1/$(spack find --format '{hash:7}' netcdf-c@4.8.1 % ${SPACK_COMPILER} +mpi ^openmpi@4.1.3)"
+declare -xr SPACK_SPEC="${SPACK_PACKAGE} % ${SPACK_COMPILER} ${SPACK_VARIANTS} ${SPACK_DEPENDENCIES}"
+
+printenv
+
+spack config get compilers
+spack config get config  
+spack config get mirrors
+spack config get modules
+spack config get packages
+spack config get repos
+spack config get upstreams
+
+spack spec --long --namespaces --types paraview@5.9.1 % gcc@10.2.0 ~adios2 ~advanced_debug +cuda cuda_arch=70 ~development_files +examples +hdf5 ~ipo ~kits +mpi +opengl2 ~osmesa ~python +python3 ~qt ~shared "^hdf5@1.10.7/$(spack find --format '{hash:7}' hdf5@1.10.7 % ${SPACK_COMPILER} +mpi ^openmpi@4.1.3) ^py-matplotlib@3.4.3/$(spack find --format '{hash:7}' py-matplotlib@3.4.3 % ${SPACK_COMPILER}) ^netcdf-c@4.8.1/$(spack find --format '{hash:7}' netcdf-c@4.8.1 % ${SPACK_COMPILER} +mpi ^openmpi@4.1.3)"
+if [[ "${?}" -ne 0 ]]; then
+  echo 'ERROR: spack concretization failed.'
+  exit 1
+fi
+
+time -p spack install --jobs "${SLURM_CPUS_PER_TASK}" --fail-fast --yes-to-all paraview@5.9.1 % gcc@10.2.0 ~adios2 ~advanced_debug +cuda cuda_arch=70 ~development_files +examples +hdf5 ~ipo ~kits +mpi +opengl2 ~osmesa ~python +python3 ~qt ~shared "^hdf5@1.10.7/$(spack find --format '{hash:7}' hdf5@1.10.7 % ${SPACK_COMPILER} +mpi ^openmpi@4.1.3) ^py-matplotlib@3.4.3/$(spack find --format '{hash:7}' py-matplotlib@3.4.3 % ${SPACK_COMPILER}) ^netcdf-c@4.8.1/$(spack find --format '{hash:7}' netcdf-c@4.8.1 % ${SPACK_COMPILER} +mpi ^openmpi@4.1.3)"
+if [[ "${?}" -ne 0 ]]; then
+  echo 'ERROR: spack install failed.'
+  exit 1
+fi
+
+spack module lmod refresh --delete-tree -y
+
+#sbatch --dependency="afterok:${SLURM_JOB_ID}" 'visit@3.1.1.sh'
+#sbatch --dependency="afterok:${SLURM_JOB_ID}" 'openfoam@2106.sh'
+
+sleep 60
