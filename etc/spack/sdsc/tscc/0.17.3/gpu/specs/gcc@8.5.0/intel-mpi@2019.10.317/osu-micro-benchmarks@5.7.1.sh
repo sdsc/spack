eac13fff825ab8ed9041f1886d79d1ac90412e4c
jpg <jpg@gpu1.cm.cluster>
2023-03-27 15:14:05 -0700

    checkin renaming gpu scripts to 0.17.3

--- etc/spack/sdsc/tscc/0.17.3/gpu/specs/gcc@8.5.0/intel-mpi@2019.10.317/osu-micro-benchmarks@5.7.1.sh
+++ etc/spack/sdsc/tscc/0.17.3/gpu/specs/gcc@8.5.0/intel-mpi@2019.10.317/osu-micro-benchmarks@5.7.1.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-#SBATCH --job-name=nvhpc@21.3
+#SBATCH --job-name=osu-micro-benchmarks@5.7.1
 #SBATCH --account=sdsc
 #SBATCH --partition=hotel
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=16
-#SBATCH --mem=32G
-#SBATCH --time=01:00:00
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=93G
+#SBATCH --gpus=1
+#SBATCH --time=00:30:00
 #SBATCH --output=%x.o%j.%N
 
 declare -xr LOCAL_TIME="$(date +'%Y%m%dT%H%M%S%z')"
@@ -16,7 +17,7 @@ declare -xir UNIX_TIME="$(date +'%s')"
 declare -xr SYSTEM_NAME='tscc'
 
 declare -xr SPACK_VERSION='0.17.3'
-declare -xr SPACK_INSTANCE_NAME='cpu'
+declare -xr SPACK_INSTANCE_NAME='gpu'
 declare -xr SPACK_INSTANCE_DIR="${HOME}/cm/shared/apps/spack/${SPACK_VERSION}/${SPACK_INSTANCE_NAME}"
 
 declare -xr SLURM_JOB_SCRIPT="$(scontrol show job ${SLURM_JOB_ID} | awk -F= '/Command=/{print $2}')"
@@ -34,10 +35,10 @@ module load "${SCHEDULER_MODULE}"
 module list
 . "${SPACK_INSTANCE_DIR}/share/spack/setup-env.sh"
 
-declare -xr SPACK_PACKAGE='nvhpc@21.3'
-declare -xr SPACK_COMPILER='gcc@11.2.0'
-declare -xr SPACK_VARIANTS='+mpi'
-declare -xr SPACK_DEPENDENCIES=''
+declare -xr SPACK_PACKAGE='osu-micro-benchmarks@5.7.1'
+declare -xr SPACK_COMPILER='gcc@8.5.0'
+declare -xr SPACK_VARIANTS='~cuda'
+declare -xr SPACK_DEPENDENCIES="^intel-mpi@2019.10.317/$(spack find --format '{hash:7}' intel-mpi@2019.10.317 % ${SPACK_COMPILER})"
 declare -xr SPACK_SPEC="${SPACK_PACKAGE} % ${SPACK_COMPILER} ${SPACK_VARIANTS} ${SPACK_DEPENDENCIES}"
 
 printenv
@@ -62,9 +63,8 @@ if [[ "${?}" -ne 0 ]]; then
   exit 1
 fi
 
-spack compiler add --scope site "$(spack location -i ${SPACK_PACKAGE})"
 spack module lmod refresh --delete-tree -y
 
-#sbatch --dependency="afterok:${SLURM_JOB_ID}" 'ucx@1.10.1.sh'
+sbatch --dependency="afterok:${SLURM_JOB_ID}" 'netlib-scalapack@2.1.0.sh'
 
-#sleep 60
+sleep 60
--- etc/spack/sdsc/tscc/0.17.3/gpu/specs/gcc@8.5.0/intel-mpi@2019.10.317/py-netcdf4@1.5.3.sh
+++ etc/spack/sdsc/tscc/0.17.3/gpu/specs/gcc@8.5.0/intel-mpi@2019.10.317/py-netcdf4@1.5.3.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-#SBATCH --job-name=nvhpc@21.3
+#SBATCH --job-name=py-netcdf4@1.5.3
 #SBATCH --account=sdsc
 #SBATCH --partition=hotel
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=16
-#SBATCH --mem=32G
-#SBATCH --time=01:00:00
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=93G
+#SBATCH --gpus=1
+#SBATCH --time=00:30:00
 #SBATCH --output=%x.o%j.%N
 
 declare -xr LOCAL_TIME="$(date +'%Y%m%dT%H%M%S%z')"
@@ -16,7 +17,7 @@ declare -xir UNIX_TIME="$(date +'%s')"
 declare -xr SYSTEM_NAME='tscc'
 
 declare -xr SPACK_VERSION='0.17.3'
-declare -xr SPACK_INSTANCE_NAME='cpu'
+declare -xr SPACK_INSTANCE_NAME='gpu'
 declare -xr SPACK_INSTANCE_DIR="${HOME}/cm/shared/apps/spack/${SPACK_VERSION}/${SPACK_INSTANCE_NAME}"
 
 declare -xr SLURM_JOB_SCRIPT="$(scontrol show job ${SLURM_JOB_ID} | awk -F= '/Command=/{print $2}')"
@@ -34,10 +35,10 @@ module load "${SCHEDULER_MODULE}"
 module list
 . "${SPACK_INSTANCE_DIR}/share/spack/setup-env.sh"
 
-declare -xr SPACK_PACKAGE='nvhpc@21.3'
-declare -xr SPACK_COMPILER='gcc@11.2.0'
+declare -xr SPACK_PACKAGE='py-netcdf4@1.5.3'
+declare -xr SPACK_COMPILER='gcc@8.5.0'
 declare -xr SPACK_VARIANTS='+mpi'
-declare -xr SPACK_DEPENDENCIES=''
+declare -xr SPACK_DEPENDENCIES="^py-numpy@1.20.3/$(spack find --format '{hash:7}' py-numpy@1.20.3 % ${SPACK_COMPILER}) ^openblas@0.3.17/$(spack find --format '{hash:7}' openblas@0.3.17 % ${SPACK_COMPILER} ~ilp64 threads=none) ^netcdf-c@4.8.1/$(spack find --format '{hash:7}' netcdf-c@4.8.1 % ${SPACK_COMPILER} +mpi ^intel-mpi@2019.10.317)"
 declare -xr SPACK_SPEC="${SPACK_PACKAGE} % ${SPACK_COMPILER} ${SPACK_VARIANTS} ${SPACK_DEPENDENCIES}"
 
 printenv
@@ -62,9 +63,8 @@ if [[ "${?}" -ne 0 ]]; then
   exit 1
 fi
 
-spack compiler add --scope site "$(spack location -i ${SPACK_PACKAGE})"
 spack module lmod refresh --delete-tree -y
 
-#sbatch --dependency="afterok:${SLURM_JOB_ID}" 'ucx@1.10.1.sh'
+sbatch --dependency="afterok:${SLURM_JOB_ID}" 'arpack-ng@3.7.0.sh'
 
-#sleep 60
+sleep 60
