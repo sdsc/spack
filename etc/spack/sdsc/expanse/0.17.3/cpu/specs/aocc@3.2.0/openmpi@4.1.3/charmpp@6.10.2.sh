e60715a87188aabd49ee30fd198dedaaba00aaed
Marty Kandes <mkandes@sdsc.edu>
2023-04-20 09:55:19 -0700

    rm namd@2.14 % aocc@3.2.0 ^openmpi@4.1.3 from pkg dep chain in expanse/0.17.3/cpu

--- etc/spack/sdsc/expanse/0.17.3/cpu/specs/aocc@3.2.0/openmpi@4.1.3/charmpp@6.10.2.sh
+++ etc/spack/sdsc/expanse/0.17.3/cpu/specs/aocc@3.2.0/openmpi@4.1.3/charmpp@6.10.2.sh
@@ -2,8 +2,8 @@
 
 #SBATCH --job-name=charmpp@6.10.2
 #SBATCH --account=use300
-##SBATCH --reservation=root_73
-#SBATCH --partition=shared
+#SBATCH --reservation=rocky8u7_testing
+#SBATCH --partition=ind-shared
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=16
@@ -15,15 +15,14 @@ declare -xr LOCAL_TIME="$(date +'%Y%m%dT%H%M%S%z')"
 declare -xir UNIX_TIME="$(date +'%s')"
 
 declare -xr LOCAL_SCRATCH_DIR="/scratch/${USER}/job_${SLURM_JOB_ID}"
-declare -xr TMPDIR="${LOCAL_SCRATCH_DIR}/spack-stage"
-mkdir -p "${TMPDIR}"
-# -
+declare -xr TMPDIR="${LOCAL_SCRATCH_DIR}"
+
 declare -xr SYSTEM_NAME='expanse'
 
 declare -xr SPACK_VERSION='0.17.3'
 declare -xr SPACK_INSTANCE_NAME='cpu'
 declare -xr SPACK_INSTANCE_VERSION='b'
-declare -xr SPACK_INSTANCE_DIR="${HOME}/cm/shared/apps/spack/${SPACK_VERSION}/${SPACK_INSTANCE_NAME}/${SPACK_INSTANCE_VERSION}"
+declare -xr SPACK_INSTANCE_DIR="/cm/shared/apps/spack/${SPACK_VERSION}/${SPACK_INSTANCE_NAME}/${SPACK_INSTANCE_VERSION}"
 
 declare -xr SLURM_JOB_SCRIPT="$(scontrol show job ${SLURM_JOB_ID} | awk -F= '/Command=/{print $2}')"
 declare -xr SLURM_JOB_MD5SUM="$(md5sum ${SLURM_JOB_SCRIPT})"
@@ -56,8 +55,8 @@ spack config get packages
 spack config get repos
 spack config get upstreams
 
-#spack spec --long --namespaces --types "${SPACK_SPEC}"
-spack spec --long --namespaces --types charmpp@6.10.2 % aocc@3.2.0 backend=mpi build-target=charm++ ~cuda +omp ~papi pmi=none +production ~pthreads +shared +smp ~syncft ~tcp ~tracing "${SPACK_DEPENDENCIES}"
+#time -p spack spec --long --namespaces --types "${SPACK_SPEC}"
+time -p spack spec --long --namespaces --types charmpp@6.10.2 % aocc@3.2.0 backend=mpi build-target=charm++ ~cuda +omp ~papi pmi=none +production ~pthreads +shared +smp ~syncft ~tcp ~tracing "${SPACK_DEPENDENCIES}"
 if [[ "${?}" -ne 0 ]]; then
   echo 'ERROR: spack concretization failed.'
   exit 1
@@ -70,8 +69,8 @@ if [[ "${?}" -ne 0 ]]; then
   exit 1
 fi
 
-#spack module lmod refresh --delete-tree -y
+##spack module lmod refresh --delete-tree -y
 
 sbatch --dependency="afterok:${SLURM_JOB_ID}" 'namd@2.14.sh'
 
-sleep 60
+sleep 30
