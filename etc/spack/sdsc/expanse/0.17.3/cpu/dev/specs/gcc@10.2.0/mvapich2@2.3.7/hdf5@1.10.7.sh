781d351373c285ebd42d52970c100fa6dca1cb53
Marty Kandes <mkandes@sdsc.edu>
2024-04-22 11:39:29 -0700

    Add parallel-netcdf@1.12.2 % gcc@10.2.0 ^mvpaich2@2.3.7 to exp/0.17.3/cpu/dev

--- etc/spack/sdsc/expanse/0.17.3/cpu/dev/specs/gcc@10.2.0/mvapich2@2.3.7/hdf5@1.10.7.sh
+++ etc/spack/sdsc/expanse/0.17.3/cpu/dev/specs/gcc@10.2.0/mvapich2@2.3.7/hdf5@1.10.7.sh
@@ -77,6 +77,6 @@ if [[ "${?}" -ne 0 ]]; then
   exit 1
 fi
 
-#sbatch --dependency="afterok:${SLURM_JOB_ID}" ''
+sbatch --dependency="afterok:${SLURM_JOB_ID}" 'parallel-netcdf@1.12.2.sh'
 
 sleep 30
