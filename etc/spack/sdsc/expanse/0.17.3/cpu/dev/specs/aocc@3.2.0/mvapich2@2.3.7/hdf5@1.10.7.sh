241f59d98e846463549f0742191b29359dec1c47
Marty Kandes <mkandes@sdsc.edu>
2024-04-24 08:26:42 -0700

    Add hdf5@1.10.7 % aocc@3.2.0 ^mvapich2@2.3.7 to exp/0.17.3/cpu/dev

--- etc/spack/sdsc/expanse/0.17.3/cpu/dev/specs/aocc@3.2.0/mvapich2@2.3.7/hdf5@1.10.7.sh
+++ etc/spack/sdsc/expanse/0.17.3/cpu/dev/specs/aocc@3.2.0/mvapich2@2.3.7/hdf5@1.10.7.sh
@@ -48,7 +48,7 @@ module list
 . "${SPACK_INSTANCE_DIR}/share/spack/setup-env.sh"
 
 declare -xr SPACK_PACKAGE='hdf5@1.10.7'
-declare -xr SPACK_COMPILER='gcc@10.2.0'
+declare -xr SPACK_COMPILER='aocc@3.2.0'
 declare -xr SPACK_VARIANTS='+cxx +fortran +hl ~ipo +java +mpi +shared +szip ~threadsafe +tools'
 declare -xr SPACK_DEPENDENCIES="^mvapich2@2.3.7/$(spack find --format '{hash:7}' mvapich2@2.3.7 % ${SPACK_COMPILER})"
 declare -xr SPACK_SPEC="${SPACK_PACKAGE} % ${SPACK_COMPILER} ${SPACK_VARIANTS} ${SPACK_DEPENDENCIES}"
@@ -77,6 +77,6 @@ if [[ "${?}" -ne 0 ]]; then
   exit 1
 fi
 
-sbatch --dependency="afterok:${SLURM_JOB_ID}" 'parallel-netcdf@1.12.2.sh'
+#sbatch --dependency="afterok:${SLURM_JOB_ID}" ''
 
 sleep 30
