a22ffcaf5bbd63074fe362310d9be2a6ca62bf38
Marty Kandes <mkandes@sdsc.edu>
2024-05-05 09:12:34 -0700

    Add cgal@4.13 % aocc@3.2.0 to exp/0.17.3/cpu/dev

--- etc/spack/sdsc/expanse/0.17.3/cpu/dev/specs/aocc@3.2.0/cgal@4.13.sh
+++ etc/spack/sdsc/expanse/0.17.3/cpu/dev/specs/aocc@3.2.0/cgal@4.13.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#SBATCH --job-name=scotch@6.1.1
+#SBATCH --job-name=cgal@4.13
 #SBATCH --account=use300
 #SBATCH --clusters=expanse
 #SBATCH --partition=ind-shared
@@ -47,10 +47,10 @@ module load "${SCHEDULER_MODULE}"
 module list
 . "${SPACK_INSTANCE_DIR}/share/spack/setup-env.sh"
 
-declare -xr SPACK_PACKAGE='scotch@6.1.1'
-declare -xr SPACK_COMPILER='gcc@10.2.0'
-declare -xr SPACK_VARIANTS='+compression +esmumps ~int64 ~metis +mpi +shared'
-declare -xr SPACK_DEPENDENCIES="^openmpi@4.1.3/$(spack find --format '{hash:7}' openmpi@4.1.3 % ${SPACK_COMPILER})"
+declare -xr SPACK_PACKAGE='cgal@4.13'
+declare -xr SPACK_COMPILER='aocc@3.2.0'
+declare -xr SPACK_VARIANTS='+core ~demos +eigen +imageio ~ipo +shared'
+declare -xr SPACK_DEPENDENCIES="^boost@1.77.0/$(spack find --format '{hash:7}' boost@1.77.0 % ${SPACK_COMPILER} ~mpi) ^eigen@3.4.0/$(spack find --format '{hash:7}' eigen@3.4.0 % ${SPACK_COMPILER})"
 declare -xr SPACK_SPEC="${SPACK_PACKAGE} % ${SPACK_COMPILER} ${SPACK_VARIANTS} ${SPACK_DEPENDENCIES}"
 
 printenv
