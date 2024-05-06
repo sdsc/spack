776853ac85ce69ce554e6d84581654e8ae049a5a
Marty Kandes <mkandes@sdsc.edu>
2024-05-06 16:58:14 -0700

    Add beast2@2.6.4 % gcc@8.5.0 to exp/0.17.3/cpu/dev

--- etc/spack/sdsc/expanse/0.17.3/cpu/dev/specs/beast2@2.6.4.sh
+++ etc/spack/sdsc/expanse/0.17.3/cpu/dev/specs/beast2@2.6.4.sh
@@ -48,7 +48,7 @@ module list
 . "${SPACK_INSTANCE_DIR}/share/spack/setup-env.sh"
 
 declare -xr SPACK_PACKAGE='beast2@2.6.4'
-declare -xr SPACK_COMPILER='gcc@10.2.0'
+declare -xr SPACK_COMPILER='gcc@8.5.0'
 declare -xr SPACK_VARIANTS=''
 declare -xr SPACK_DEPENDENCIES=''
 declare -xr SPACK_SPEC="${SPACK_PACKAGE} % ${SPACK_COMPILER} ${SPACK_VARIANTS} ${SPACK_DEPENDENCIES}"
@@ -76,7 +76,3 @@ if [[ "${?}" -ne 0 ]]; then
   echo 'ERROR: spack install failed.'
   exit 1
 fi
-
-#sbatch --dependency="afterok:${SLURM_JOB_ID}" ''
-
-sleep 30
