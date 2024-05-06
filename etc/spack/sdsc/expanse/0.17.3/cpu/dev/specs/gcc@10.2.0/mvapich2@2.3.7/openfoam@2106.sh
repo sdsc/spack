3216a48644570c54296f7e0699a8a4197e246f6c
Marty Kandes <mkandes@sdsc.edu>
2024-05-06 12:13:35 -0700

    Add openfoam@2106 % gcc@10.2.0 ^mvapich2@2.3.7 to exp/0.17.3/cpu/dev

--- etc/spack/sdsc/expanse/0.17.3/cpu/dev/specs/gcc@10.2.0/mvapich2@2.3.7/openfoam@2106.sh
+++ etc/spack/sdsc/expanse/0.17.3/cpu/dev/specs/gcc@10.2.0/mvapich2@2.3.7/openfoam@2106.sh
@@ -50,7 +50,7 @@ module list
 declare -xr SPACK_PACKAGE='openfoam@2106'
 declare -xr SPACK_COMPILER='gcc@10.2.0'
 declare -xr SPACK_VARIANTS='~float32 ~int64 +kahip ~knl +metis ~mgridgen ~paraview +scotch +source ~spdp ~vtk +zoltan'
-declare -xr SPACK_DEPENDENCIES="^openmpi@4.1.3/$(spack find --format '{hash:7}' openmpi@4.1.3 % ${SPACK_COMPILER}) ^adios2@2.7.1/$(spack find --format '{hash:7}' adios2@2.7.1 % ${SPACK_COMPILER} +mpi ^openmpi@4.1.3) ^cgal@4.13/$(spack find --format '{hash:7}' cgal@4.13 % ${SPACK_COMPILER}) ^fftw@3.3.10/$(spack find --format '{hash:7}' fftw@3.3.10 % ${SPACK_COMPILER} ~mpi ~openmp) ^kahip@3.11/$(spack find --format '{hash:7}' kahip@3.11 % ${SPACK_COMPILER} ^openmpi@4.1.3) ^scotch@6.1.1/$(spack find --format '{hash:7}' scotch@6.1.1 % ${SPACK_COMPILER} +mpi ^openmpi@4.1.3) ^zoltan@3.83/$(spack find --format '{hash:7}' zoltan@3.83 % ${SPACK_COMPILER} +fortran +mpi +parmetis ^openmpi@4.1.3)"
+declare -xr SPACK_DEPENDENCIES="^mvapich2@2.3.7/$(spack find --format '{hash:7}' mvapich2@2.3.7 % ${SPACK_COMPILER}) ^adios2@2.7.1/$(spack find --format '{hash:7}' adios2@2.7.1 % ${SPACK_COMPILER} +mpi ^mvapich2@2.3.7) ^cgal@4.13/$(spack find --format '{hash:7}' cgal@4.13 % ${SPACK_COMPILER}) ^fftw@3.3.10/$(spack find --format '{hash:7}' fftw@3.3.10 % ${SPACK_COMPILER} ~mpi ~openmp) ^kahip@3.11/$(spack find --format '{hash:7}' kahip@3.11 % ${SPACK_COMPILER} ^mvapich2@2.3.7) ^scotch@6.1.1/$(spack find --format '{hash:7}' scotch@6.1.1 % ${SPACK_COMPILER} +mpi ^mvapich2@2.3.7) ^zoltan@3.83/$(spack find --format '{hash:7}' zoltan@3.83 % ${SPACK_COMPILER} +fortran +mpi +parmetis ^mvapich2@2.3.7)"
 declare -xr SPACK_SPEC="${SPACK_PACKAGE} % ${SPACK_COMPILER} ${SPACK_VARIANTS} ${SPACK_DEPENDENCIES}"
 
 printenv
