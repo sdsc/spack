e0d2a357e93c62f79d40484579c2ae61ad72a322
Marty Kandes <mkandes@sdsc.edu>
2024-11-05 07:46:56 -0800

    Update namd@2.14 % aocc@4.2.0 to use charmpp@6.10.2 backend=verbs

--- etc/spack/sdsc/exp/cpu/dev/specs/aocc@4.2.0/namd@2.14.sh
+++ etc/spack/sdsc/exp/cpu/dev/specs/aocc@4.2.0/namd@2.14.sh
@@ -48,7 +48,7 @@ module list
 declare -xr SPACK_PACKAGE='namd@2.14'
 declare -xr SPACK_COMPILER='aocc@4.2.0'
 declare -xr SPACK_VARIANTS='~cuda fftw=amdfftw interface=tcl'
-declare -xr SPACK_DEPENDENCIES="^charmpp@6.10.2/$(spack find --format '{hash:7}' charmpp@6.10.2 % ${SPACK_COMPILER} backend=multicore) ^amdfftw@4.2/$(spack find --format '{hash:7}' amdfftw@4.2 % ${SPACK_COMPILER} ~mpi ~openmp)"
+declare -xr SPACK_DEPENDENCIES="^charmpp@6.10.2/$(spack find --format '{hash:7}' charmpp@6.10.2 % ${SPACK_COMPILER} backend=verbs) ^amdfftw@4.2/$(spack find --format '{hash:7}' amdfftw@4.2 % ${SPACK_COMPILER} ~mpi ~openmp)"
 declare -xr SPACK_SPEC="${SPACK_PACKAGE} % ${SPACK_COMPILER} ${SPACK_VARIANTS} ${SPACK_DEPENDENCIES}"
 
 printenv
