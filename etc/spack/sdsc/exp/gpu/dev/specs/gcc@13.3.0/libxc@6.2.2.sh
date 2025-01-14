0fb5a65919f5236985db03f0e39f6096ea7c974f
Marty Kandes <mkandes@sdsc.edu>
2025-01-14 15:35:41 -0800

    Update libxc@6.2.2 % gcc@13.3.0 to +cuda@12.6.3
    Note, however, the build currently fails. Revisit in next pass. Consider
    using gcc@11.2.x and cuda@11.8.0 instead.

--- etc/spack/sdsc/exp/gpu/dev/specs/gcc@13.3.0/libxc@6.2.2.sh
+++ etc/spack/sdsc/exp/gpu/dev/specs/gcc@13.3.0/libxc@6.2.2.sh
@@ -49,8 +49,8 @@ module list
 
 declare -xr SPACK_PACKAGE='libxc@6.2.2'
 declare -xr SPACK_COMPILER='gcc@13.3.0'
-declare -xr SPACK_VARIANTS='~cuda +shared'
-declare -xr SPACK_DEPENDENCIES=''
+declare -xr SPACK_VARIANTS='+cuda ~shared cuda_arch=70,80,90'
+declare -xr SPACK_DEPENDENCIES="^cuda@12.6.3/$(spack find --format '{hash:7}' cuda@12.6.3 % ${SPACK_COMPILER})"
 declare -xr SPACK_SPEC="${SPACK_PACKAGE} % ${SPACK_COMPILER} ${SPACK_VARIANTS} ${SPACK_DEPENDENCIES}"
 
 printenv
