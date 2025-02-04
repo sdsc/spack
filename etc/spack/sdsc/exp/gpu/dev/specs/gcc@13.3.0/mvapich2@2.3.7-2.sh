78583fa510cdef991370cc509e1c6ee7bbfef9ca
Marty Kandes <mkandes@sdsc.edu>
2025-02-03 17:12:45 -0800

    Update mvapich2@2.3.7-2 % gcc@13.3.0 to include +cuda extension
    Note, however, mvapich2 still fails to build due to a linking problem
    with the copy of OMB included in the install [1]. We will try and
    revisit on the next pass through the stack.
    
    [1]
    
    ==> Installing mvapich2-2.3.7-2-cqmjqqmxyeudpgdsrjcghtdetgropner [26/26]
    ==> No binary for mvapich2-2.3.7-2-cqmjqqmxyeudpgdsrjcghtdetgropner found: installing from source
    ==> Using cached archive: /home/mkandes/software/spack/repos/sdsc/gpu/var/spack/cache/_source-cache/archive/03/03fc160c81a1d522b1e7c6723841e27fcab04d527484e4242d3e66d7c5024205.tar.gz
    ==> Applied patch /home/mkandes/software/spack/repos/sdsc/gpu/var/spack/repos/sdsc/packages/mvapich2/mpir_attr_delete_list_segfault.patch
    ==> mvapich2: Executing phase: 'autoreconf'
    ==> mvapich2: Executing phase: 'configure'
    ==> Error: ProcessError: Command exited with status 1:
        '/scratch/mkandes/job_36719718/spack-stage/spack-stage/spack-stage-mvapich2-2.3.7-2-cqmjqqmxyeudpgdsrjcghtdetgropner/spack-src/configure' '--prefix=/home/mkandes/software/spack/repos/sdsc/gpu/opt/spack/linux-rocky8-cascadelake/gcc-13.3.0/mvapich2-2.3.7-2-cqmjqqmxyeudpgdsrjcghtdetgropner' '--enable-shared' '--enable-romio' '--disable-silent-rules' '--disable-new-dtags' '--enable-fortran=all' '--enable-threads=multiple' '--with-ch3-rank-bits=32' '--enable-wrapper-rpath=yes' '--disable-alloca' '--enable-fast=all' '--enable-cuda' '--with-cuda=/home/mkandes/software/spack/repos/sdsc/gpu/opt/spack/linux-rocky8-cascadelake/gcc-13.3.0/cuda-12.6.3-guub3z6c6k7zgb3qv3vdngxr6zu5mhzy' '--disable-opencl' '--disable-gl' '--disable-nvml' '--enable-registration-cache' '--with-pmi=pmi2' '--with-pm=slurm' '--with-slurm=/cm/shared/apps/slurm/current' '--with-device=ch3:mrail' '--with-rdma=gen2' '--disable-mcast' '--with-file-system=lustre'
    
    2 errors found in build log:
         575    checking for inline... inline
         576    checking for getpagesize... yes
         577    checking for gettimeofday... yes
         578    checking for memset... yes
         579    checking for sqrt... yes
         580    checking for library containing cuPointerGetAttribute... no
      >> 581    configure: error: cannot link with -lcuda
      >> 582    configure: error: OMB configure failed
    
    See build log for details:
      /scratch/mkandes/job_36719718/spack-stage/spack-stage/spack-stage-mvapich2-2.3.7-2-cqmjqqmxyeudpgdsrjcghtdetgropner/spack-build-out.txt

--- etc/spack/sdsc/exp/gpu/dev/specs/gcc@13.3.0/mvapich2@2.3.7-2.sh
+++ etc/spack/sdsc/exp/gpu/dev/specs/gcc@13.3.0/mvapich2@2.3.7-2.sh
@@ -28,6 +28,20 @@ declare -xr SCHEDULER_REVISION='7'
 declare -xr SCHEDULER_VERSION="${SCHEDULER_MAJOR}.${SCHEDULER_MINOR}.${SCHEDULER_REVISION}"
 declare -xr SCHEDULER_MODULE="${SCHEDULER_NAME}/${SLURM_CLUSTER_NAME}/${SCHEDULER_VERSION}"
 
+declare -xr COMPILER_NAME='gcc'
+declare -xr COMPILER_MAJOR='13'
+declare -xr COMPILER_MINOR='3'
+declare -xr COMPILER_REVISION='0'
+declare -xr COMPILER_VERSION="${COMPILER_MAJOR}.${COMPILER_MINOR}.${COMPILER_REVISION}"
+declare -xr COMPILER_MODULE="${COMPILER_NAME}/${COMPILER_VERSION}"
+
+declare -xr CUDA_NAME='cuda'
+declare -xr CUDA_MAJOR='12'
+declare -xr CUDA_MINOR='6'
+declare -xr CUDA_REVISION='3'
+declare -xr CUDA_VERSION="${CUDA_MAJOR}.${CUDA_MINOR}.${CUDA_REVISION}"
+declare -xr CUDA_MODULE="${CUDA_NAME}/${CUDA_VERSION}"
+
 declare -xr SPACK_MAJOR='0'
 declare -xr SPACK_MINOR='21'
 declare -xr SPACK_REVISION='2'
@@ -44,13 +58,16 @@ cat  "${JOB_SCRIPT}"
 
 module purge
 module load "${SCHEDULER_MODULE}"
-module list
 . "${SPACK_INSTANCE_DIR}/share/spack/setup-env.sh"
+module use "${SPACK_ROOT}/share/spack/lmod/linux-rocky8-x86_64/Core"
+module load "${COMPILER_MODULE}"
+module load "${CUDA_MODULE}"
+module list
 
 declare -xr SPACK_PACKAGE='mvapich2@2.3.7-2'
 declare -xr SPACK_COMPILER='gcc@13.3.0'
-declare -xr SPACK_VARIANTS='~alloca ch3_rank_bits=32 ~cuda ~debug fabrics=mrail file_systems=lustre ~hwloc_graphics ~hwlocv2 process_managers=slurm +regcache threads=multiple +wrapperrpath'
-declare -xr SPACK_DEPENDENCIES='^slurm ^rdma-core'
+declare -xr SPACK_VARIANTS='~alloca ch3_rank_bits=32 +cuda ~debug fabrics=mrail file_systems=lustre ~hwloc_graphics ~hwlocv2 process_managers=slurm +regcache threads=multiple +wrapperrpath'
+declare -xr SPACK_DEPENDENCIES="^slurm ^rdma-core ^cuda@12.6.3/$(spack find --format '{hash:7}' cuda@12.6.3 % ${SPACK_COMPILER})"
 declare -xr SPACK_SPEC="${SPACK_PACKAGE} % ${SPACK_COMPILER} ${SPACK_VARIANTS} ${SPACK_DEPENDENCIES}"
 
 printenv
