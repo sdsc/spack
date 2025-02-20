1647fed008bfb359d24b4b2ec534357dee117f05
Marty Kandes <mkandes@sdsc.edu>
2025-02-20 10:10:40 -0800

    Add gromacs@2024.3 % gcc@13.3.0 +mpi +cuda +plumed ^openmpi@4.1.6

--- etc/spack/sdsc/exp/gpu/dev/specs/gcc@13.3.0/openmpi@4.1.6/gromacs@2024.3.sh
+++ etc/spack/sdsc/exp/gpu/dev/specs/gcc@13.3.0/openmpi@4.1.6/gromacs@2024.3.sh
@@ -49,9 +49,9 @@ module list
 
 declare -xr SPACK_PACKAGE='gromacs@2024.3'
 declare -xr SPACK_COMPILER='gcc@13.3.0'
-declare -xr SPACK_VARIANTS='~cp2k ~cuda ~cycle_subcounters ~double +hwloc ~intel_provided_gcc ~ipo ~mdrun_only +mpi ~nosuffix ~opencl +openmp ~relaxed_double_precision +shared ~sycl'
+declare -xr SPACK_VARIANTS='~cp2k +cuda cuda_arch=70,80,90 ~cycle_subcounters ~double +hwloc ~intel_provided_gcc ~ipo ~mdrun_only +mpi ~nosuffix ~opencl +openmp +plumed ~relaxed_double_precision +shared ~sycl'
 declare -xr SPACK_MPI='openmpi@4.1.6'
-declare -xr SPACK_DEPENDENCIES="^openblas@0.3.28/$(spack find --format '{hash:7}' openblas@0.3.28 % ${SPACK_COMPILER} ~ilp64 threads=none) ^fftw@3.3.10/$(spack find --format '{hash:7}' fftw@3.3.10 % ${SPACK_COMPILER} ~mpi ~openmp) ^${SPACK_MPI}/$(spack find --format '{hash:7}' ${SPACK_MPI} % ${SPACK_COMPILER})" 
+declare -xr SPACK_DEPENDENCIES="^openblas@0.3.28/$(spack find --format '{hash:7}' openblas@0.3.28 % ${SPACK_COMPILER} ~ilp64 threads=none) ^fftw@3.3.10/$(spack find --format '{hash:7}' fftw@3.3.10 % ${SPACK_COMPILER} ~mpi ~openmp) ^${SPACK_MPI}/$(spack find --format '{hash:7}' ${SPACK_MPI} % ${SPACK_COMPILER} +cuda) ^plumed@2.9.3/$(spack find --format '{hash:7}' plumed@2.9.3 % ${SPACK_COMPILER} +mpi ^${SPACK_MPI})" 
 declare -xr SPACK_SPEC="${SPACK_PACKAGE} % ${SPACK_COMPILER} ${SPACK_VARIANTS} ${SPACK_DEPENDENCIES}"
 
 printenv
