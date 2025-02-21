4325897dc68e8d51be9bc1a75fd5e554e920c6eb
Marty Kandes <mkandes@sdsc.edu>
2025-02-20 20:05:39 -0800

    Reconcile minor changes between % gcc and % intel specs for a few pkgs

--- etc/spack/sdsc/exp/gpu/dev/specs/gcc@13.3.0/libbeagle@3.1.2.sh
+++ etc/spack/sdsc/exp/gpu/dev/specs/gcc@13.3.0/libbeagle@3.1.2.sh
@@ -49,7 +49,7 @@ module list
 
 declare -xr SPACK_PACKAGE='libbeagle@3.1.2'
 declare -xr SPACK_COMPILER='gcc@13.3.0'
-declare -xr SPACK_VARIANTS='+cuda ~opencl cuda_arch=70'
+declare -xr SPACK_VARIANTS='+cuda cuda_arch=70 ~opencl'
 declare -xr SPACK_DEPENDENCIES="^cuda@12.6.3/$(spack find --format '{hash:7}' cuda@12.6.3 % ${SPACK_COMPILER})"
 declare -xr SPACK_SPEC="${SPACK_PACKAGE} % ${SPACK_COMPILER} ${SPACK_VARIANTS} ${SPACK_DEPENDENCIES}"
 
--- etc/spack/sdsc/exp/gpu/dev/specs/gcc@13.3.0/libxc@6.2.2.sh
+++ etc/spack/sdsc/exp/gpu/dev/specs/gcc@13.3.0/libxc@6.2.2.sh
@@ -49,7 +49,7 @@ module list
 
 declare -xr SPACK_PACKAGE='libxc@6.2.2'
 declare -xr SPACK_COMPILER='gcc@13.3.0'
-declare -xr SPACK_VARIANTS='+cuda ~shared cuda_arch=70,80,90'
+declare -xr SPACK_VARIANTS='+cuda cuda_arch=70,80,90 ~shared'
 declare -xr SPACK_DEPENDENCIES="^cuda@12.6.3/$(spack find --format '{hash:7}' cuda@12.6.3 % ${SPACK_COMPILER})"
 declare -xr SPACK_SPEC="${SPACK_PACKAGE} % ${SPACK_COMPILER} ${SPACK_VARIANTS} ${SPACK_DEPENDENCIES}"
 
