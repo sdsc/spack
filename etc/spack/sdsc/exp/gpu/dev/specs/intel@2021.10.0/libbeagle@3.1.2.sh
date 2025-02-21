902fbb4ee885fc09b287a2558edadf235c4342c7
Marty Kandes <mkandes@sdsc.edu>
2025-02-20 19:22:22 -0800

    Update libbeagle@3.1.2 % intel@2021.10.0 to +cuda@12.6.3 cuda_arch=70

--- etc/spack/sdsc/exp/gpu/dev/specs/intel@2021.10.0/libbeagle@3.1.2.sh
+++ etc/spack/sdsc/exp/gpu/dev/specs/intel@2021.10.0/libbeagle@3.1.2.sh
@@ -49,8 +49,8 @@ module list
 
 declare -xr SPACK_PACKAGE='libbeagle@3.1.2'
 declare -xr SPACK_COMPILER='intel@2021.10.0'
-declare -xr SPACK_VARIANTS='~cuda ~opencl'
-declare -xr SPACK_DEPENDENCIES=''
+declare -xr SPACK_VARIANTS='+cuda cuda_arch=70 ~opencl'
+declare -xr SPACK_DEPENDENCIES="^cuda@12.6.3/$(spack find --format '{hash:7}' cuda@12.6.3 % ${SPACK_COMPILER})"
 declare -xr SPACK_SPEC="${SPACK_PACKAGE} % ${SPACK_COMPILER} ${SPACK_VARIANTS} ${SPACK_DEPENDENCIES}"
 
 printenv
