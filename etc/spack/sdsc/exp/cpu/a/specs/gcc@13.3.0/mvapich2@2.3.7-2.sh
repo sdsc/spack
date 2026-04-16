ba67197942bf591d1ce655871a33d66ac447e7ad
Marty Kandes <mkandes@sdsc.edu>
2026-04-15 17:59:08 -0700

    Replace mvapich2@2.3.7-2 % * for exp/cpu/a with +hwlocv2

--- etc/spack/sdsc/exp/cpu/a/specs/gcc@13.3.0/mvapich2@2.3.7-2.sh
+++ etc/spack/sdsc/exp/cpu/a/specs/gcc@13.3.0/mvapich2@2.3.7-2.sh
@@ -49,7 +49,7 @@ module list
 
 declare -xr SPACK_PACKAGE='mvapich2@2.3.7-2'
 declare -xr SPACK_COMPILER='gcc@13.3.0'
-declare -xr SPACK_VARIANTS='~alloca ch3_rank_bits=32 ~cuda ~debug fabrics=mrail file_systems=lustre ~hwloc_graphics ~hwlocv2 process_managers=slurm +regcache threads=multiple +wrapperrpath'
+declare -xr SPACK_VARIANTS='~alloca ch3_rank_bits=32 ~cuda ~debug fabrics=mrail file_systems=lustre ~hwloc_graphics +hwlocv2 process_managers=slurm +regcache threads=multiple +wrapperrpath'
 declare -xr SPACK_DEPENDENCIES='^slurm ^rdma-core'
 declare -xr SPACK_SPEC="${SPACK_PACKAGE} % ${SPACK_COMPILER} ${SPACK_VARIANTS} ${SPACK_DEPENDENCIES}"
 
