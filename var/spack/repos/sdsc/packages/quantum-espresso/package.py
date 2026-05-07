d23d1d9941bf2aa0aa2c702c351369f4eafa3f5a
Marty Kandes <mkandes@sdsc.edu>
2026-05-07 12:07:39 -0700

    Add QE 7.5 to exp/gpu/a (including updating package.py)

--- var/spack/repos/sdsc/packages/quantum-espresso/package.py
+++ var/spack/repos/sdsc/packages/quantum-espresso/package.py
@@ -23,6 +23,7 @@ class QuantumEspresso(CMakePackage, Package):
     build_system(conditional("cmake", when="@6.8:"), "generic", default="cmake")
 
     version("develop", branch="develop")
+    version("7.5", sha256="7e1f7a9a21b63192f5135218bee20a5321b66582e4756536681b76e9c59b3cc8")
     version("7.4.1", sha256="6ef9c53dbf0add2a5bf5ad2a372c0bff935ad56c4472baa001003e4f932cab97")
     version("7.4", sha256="b15dcfe25f4fbf15ccd34c1194021e90996393478226e601d876f7dea481d104")
     version("7.3.1", sha256="2c58b8fadfe4177de5a8b69eba447db5e623420b070dea6fd26c1533b081d844")
